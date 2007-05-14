9727d5c8c079b29aa3dd7ae047a5eeced59f500c
cbrun <cbrun>
2007-05-14 06:31:48 +0000

    ADD adapterFinder test

---
+++ tests/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/match/statistic/test/unit/TestFindAdapterFactory.java
@@ -0,0 +1,49 @@
+package org.eclipse.emf.compare.match.statistic.test.unit;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.compare.diff.DiffFactory;
+import org.eclipse.emf.compare.diff.DiffModel;
+import org.eclipse.emf.compare.match.statistic.test.util.EMFCompareTestCase;
+import org.eclipse.emf.compare.ui.legacy.AdapterUtils;
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * Test the finding of the EMF adapterFactory
+ * 
+ * @author Cedric Brun <cedric.brun@obeo.fr>
+ * 
+ */
+public class TestFindAdapterFactory extends EMFCompareTestCase {
+
+	/**
+	 * Check the util finding the adapter factory
+	 * 
+	 * @throws Exception
+	 */
+	public void testFindAdapterFactory() throws Exception {
+		String uri = "http://www.eclipse.org/emf/compare/diff/1.0"; //$NON-NLS-1$
+		AdapterUtils util = new AdapterUtils();
+		AdapterFactory factory = util.findAdapterFactory(uri);
+		assertNotNull(factory);
+	}
+	/**
+	 * Find the adapter factory from an loaded file
+	 */
+	public void testFindAdapterFactoryFromFile() throws Exception{
+		EObject model = load(pluginFile("/data/result.diff")); //$NON-NLS-1$
+		AdapterUtils util = new AdapterUtils();		
+		AdapterFactory factory = util.findAdapterFactory(model);
+		assertNotNull(factory);
+	}
+	
+	
+	/**
+	 * Find the adapter factory from an eobject
+	 */
+	public void testFindAdapterFactoryFromEObject() throws Exception{
+		DiffModel model = DiffFactory.eINSTANCE.createDiffModel();
+		AdapterUtils util = new AdapterUtils();		
+		AdapterFactory factory = util.findAdapterFactory(model);
+		assertNotNull(factory);
+	}
+}
