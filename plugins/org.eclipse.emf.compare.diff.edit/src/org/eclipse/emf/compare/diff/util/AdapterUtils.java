ad1b2565fd38c6ca3e360a9dd6f12e05f66cedb4
cbrun <cbrun>
2007-05-30 09:36:18 +0000

    FIX : bad email format in copyright

--- plugins/org.eclipse.emf.compare.diff.edit/src/org/eclipse/emf/compare/diff/util/AdapterUtils.java
+++ plugins/org.eclipse.emf.compare.diff.edit/src/org/eclipse/emf/compare/diff/util/AdapterUtils.java
@@ -13,7 +13,7 @@ import org.eclipse.emf.ecore.EObject;
 /**
  * Usefull methods for EMF adapter factories
  * 
- * @author Cedric Brun  cedric.brun@obeo.fr 
+ * @author Cedric Brun  <a href="mailto:cedric.brun@obeo.fr ">cedric.brun@obeo.fr</a> 
  * 
  */
 public class AdapterUtils {
