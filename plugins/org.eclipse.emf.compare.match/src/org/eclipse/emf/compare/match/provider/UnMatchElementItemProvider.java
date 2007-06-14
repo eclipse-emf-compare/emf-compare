633218d857fcdcee8de7ff193de1174e52907aba
cbrun <cbrun>
2007-06-14 14:57:38 +0000

    UPDATE : fully redesigned and refactored compare, enhancements and various fixes.

--- plugins/org.eclipse.emf.compare.match/src/org/eclipse/emf/compare/match/provider/UnMatchElementItemProvider.java
+++ plugins/org.eclipse.emf.compare.match/src/org/eclipse/emf/compare/match/provider/UnMatchElementItemProvider.java
@@ -1,8 +1,12 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: UnMatchElementItemProvider.java,v 1.2 2007/06/06 08:20:00 cbrun Exp $
+/*  
+ * Copyright (c) 2006, Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
  */
 package org.eclipse.emf.compare.match.provider;
 
