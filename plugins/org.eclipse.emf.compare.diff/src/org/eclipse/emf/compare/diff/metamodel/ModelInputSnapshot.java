5bbf05824363066f2f0768d7d8d9c6a09b13b3d6
cbrun <cbrun>
2007-06-22 15:09:43 +0000

    REFACTORING : Match metamodel is now in the "metamodel" subdir.

--- plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/metamodel/ModelInputSnapshot.java
+++ plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/metamodel/ModelInputSnapshot.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: ModelInputSnapshot.java,v 1.2 2007/06/22 13:14:16 cbrun Exp $
- */
+/*******************************************************************************
+ * Copyright (c) 2006, 2007 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.compare.diff.metamodel;
 
 import java.util.Date;
--- plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/metamodel/impl/ModelInputSnapshotImpl.java
+++ plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/metamodel/impl/ModelInputSnapshotImpl.java
@@ -1,21 +1,29 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: ModelInputSnapshotImpl.java,v 1.2 2007/06/22 13:14:16 cbrun Exp $
- */
+/*******************************************************************************
+ * Copyright (c) 2006, 2007 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.compare.diff.metamodel.impl;
 
 import java.util.Date;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
+
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.compare.diff.metamodel.DiffPackage;
 import org.eclipse.emf.compare.diff.metamodel.ModelInputSnapshot;
+
 import org.eclipse.emf.compare.match.metamodel.MatchModel;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
+
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 
