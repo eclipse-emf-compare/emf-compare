292977e56f0393b97c4e10c0d29e68468d8db8d5
lgoubet <lgoubet>
2008-04-03 12:48:03 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.emf.compare.team.subversive/src/org/eclipse/emf/compare/team/subversive/SubversiveTeamHandler.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.compare.team.subversive;
+
+import java.io.IOException;
+
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.compare.structuremergeviewer.ICompareInput;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.compare.ui.internal.AbstractTeamHandler;
+import org.eclipse.emf.compare.util.ModelUtils;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.team.svn.core.connector.SVNRevision;
+import org.eclipse.team.svn.ui.compare.ResourceCompareInput.ResourceElement;
+
+/**
+ * This class will handle the specific parts of loading resources for a comparison via subversive.
+ * 
+ * @author Laurent Goubet <a href="mailto:laurent.goubet@obeo.fr">laurent.goubet@obeo.fr</a>
+ */
+public class SubversiveTeamHandler extends AbstractTeamHandler {
+	/** Indicates that the right resource is remote. */
+	private boolean rightIsRemote;
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.compare.ui.internal.AbstractTeamHandler#isRightRemote()
+	 */
+	public boolean isRightRemote() {
+		return rightIsRemote;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.compare.ui.internal.AbstractTeamHandler#loadResources(org.eclipse.compare.structuremergeviewer.ICompareInput)
+	 */
+	public boolean loadResources(ICompareInput input, ResourceSet resourceSet) throws IOException,
+			CoreException {
+		final ITypedElement left = input.getLeft();
+		final ITypedElement right = input.getRight();
+		final ITypedElement ancestor = input.getAncestor();
+
+		if (left instanceof ResourceElement && right instanceof ResourceElement) {
+			if (((ResourceElement)left).getRepositoryResource().getSelectedRevision() == SVNRevision.WORKING) {
+				rightResource = ModelUtils.load(
+						((ResourceElement)left).getLocalResource().getResource().getFullPath(), resourceSet)
+						.eResource();
+			} else {
+				rightResource = ModelUtils.load(((ResourceElement)left).getContents(),
+						((ResourceElement)left).getName(), resourceSet).eResource();
+				rightIsRemote = true;
+			}
+			leftResource = ModelUtils.load(((ResourceElement)right).getContents(),
+					((ResourceElement)right).getName(), resourceSet).eResource();
+			if (ancestor != null)
+				ancestorResource = ModelUtils.load(((ResourceElement)ancestor).getContents(),
+						((ResourceElement)ancestor).getName(), resourceSet).eResource();
+			return true;
+		}
+		return false;
+	}
+}
