5a7760d69b5d535dd90304ed8c4b945c9dcb13b8
lgoubet <lgoubet>
2008-04-15 08:59:16 +0000

    Adding package level documentation thoughout the code

---
+++ plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/api/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * (non-javadoc)
+ * Copyright (c) 2006, 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+/**
+ * This packages provides the interface that can be implemented by clients
+ * to define their own diff engine.
+ */
+package org.eclipse.emf.compare.diff.api;
---
+++ plugins/org.eclipse.emf.compare.diff/src/org/eclipse/emf/compare/diff/merge/api/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * (non-javadoc)
+ * Copyright (c) 2006, 2007, 2008 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+/**
+ * Provides the API for all of the merging features.
+ * <p>
+ * Clients should look at the documentation for the extension point
+ * <code>org.eclipse.emf.compare.diff.mergerprovider</code> as well as the
+ * documentation of 
+ * {@link org.eclipse.emf.compare.diff.merge.service.MergeService}.
+ * </p>
+ * 
+ * @since 0.8
+ */
+package org.eclipse.emf.compare.diff.merge.api;
