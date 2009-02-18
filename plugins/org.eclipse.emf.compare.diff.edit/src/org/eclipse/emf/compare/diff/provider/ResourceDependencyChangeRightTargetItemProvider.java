5ed85f5bc620bcbfdb9e4b73fbb9e1df0acff672
lgoubet <lgoubet>
2009-02-18 14:57:16 +0000

    Fixing copyrights all around

--- plugins/org.eclipse.emf.compare.diff.edit/src/org/eclipse/emf/compare/diff/provider/ResourceDependencyChangeRightTargetItemProvider.java
+++ plugins/org.eclipse.emf.compare.diff.edit/src/org/eclipse/emf/compare/diff/provider/ResourceDependencyChangeRightTargetItemProvider.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id: ResourceDependencyChangeRightTargetItemProvider.java,v 1.1 2009/02/13 11:33:33 lgoubet Exp $
- */
+/*******************************************************************************
+ * Copyright (c) 2009 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.compare.diff.provider;
 
 import java.util.Collection;
