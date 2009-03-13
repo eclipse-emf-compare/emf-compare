dade10bfed11b5caa0b8cb233f933e102788302d
cbrun <cbrun>
2009-03-13 14:10:52 +0000

    Integrate EPatch contribution #262928

---
+++ plugins/org.eclipse.emf.compare.epatch.dsl.ui/src/org/eclipse/emf/compare/epatch/dsl/EpatchUiModule.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.emf.compare.epatch.dsl;
+
+/**
+ * Use this class to register components to be used within the IDE.
+ * 
+ * @author Moritz Eysholdt - Initial contribution and API
+ */
+public class EpatchUiModule extends org.eclipse.emf.compare.epatch.dsl.AbstractEpatchUiModule {
+
+}
