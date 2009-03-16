80e12f3d677471585d32551f9bd1f7aac73f7d96
lgoubet <lgoubet>
2009-03-16 14:25:04 +0000

    Restoring backward compatibility for Europa

--- plugins/org.eclipse.emf.compare.team.subversive/src/org/eclipse/emf/compare/team/subversive/SubversiveTeamHandler.java
+++ plugins/org.eclipse.emf.compare.team.subversive/src/org/eclipse/emf/compare/team/subversive/SubversiveTeamHandler.java
@@ -10,26 +10,35 @@
  *******************************************************************************/
 package org.eclipse.emf.compare.team.subversive;
 
+import java.io.File;
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 
 import org.eclipse.compare.IStreamContentAccessor;
 import org.eclipse.compare.ITypedElement;
 import org.eclipse.compare.structuremergeviewer.ICompareInput;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.ui.team.AbstractTeamHandler;
 import org.eclipse.emf.compare.util.EclipseModelUtils;
 import org.eclipse.emf.compare.util.ModelUtils;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
+import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.resource.impl.URIHandlerImpl;
+import org.eclipse.emf.ecore.resource.impl.URIMappingRegistryImpl;
+import org.eclipse.emf.ecore.resource.impl.URIConverterImpl.URIMap;
 import org.eclipse.team.svn.core.connector.ISVNConnector;
 import org.eclipse.team.svn.core.connector.ISVNProgressMonitor;
 import org.eclipse.team.svn.core.connector.SVNConnectorException;
@@ -119,6 +128,7 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 		return false;
 	}
 
+	/* (non-javadoc) most of the behavior here has been copied from EMF 2.4 "URIHandleImpl". */
 	/**
 	 * This implementation of an URIConverter allows us to properly resolve cross-model links towards the
 	 * actual revision that should be loaded.
@@ -126,7 +136,21 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 	 * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
 	 * @since 0.9
 	 */
-	private class RevisionedURIConverter extends ExtensibleURIConverterImpl {
+	private class RevisionedURIConverter implements URIConverter {
+		/**
+		 * The URI map.
+		 */
+		protected URIMap uriMap;
+
+		/** The revision of the base model. This revision's timestamp will be used to resolve proxies. */
+		private final IRepositoryResource baseRevision;
+
+		/** The local resource currently compared. */
+		private final ILocalResource localResource;
+
+		/** Resolve the workspace root once and only. */
+		private final IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+
 		/**
 		 * This default constructor will add our own URI Handler to the top of the handlers list.
 		 * 
@@ -137,33 +161,6 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 		 */
 		public RevisionedURIConverter(IRepositoryResource revision, ILocalResource local) {
 			super();
-			uriHandlers.add(0, new RevisionedURIHandler(revision, local));
-		}
-	}
-
-	/**
-	 * This implementation of an URIHandler allows us to properly resolve cross-model links towards the actual
-	 * revision that should be loaded.
-	 * 
-	 * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
-	 * @since 0.9
-	 */
-	private class RevisionedURIHandler extends URIHandlerImpl {
-		/** The revision of the base model. This revision's timestamp will be used to resolve proxies. */
-		private final IRepositoryResource baseRevision;
-
-		/** The local resource currently compared. */
-		private final ILocalResource localResource;
-
-		/**
-		 * Instantiates an URIHandler given the base file revision.
-		 * 
-		 * @param revision
-		 *            Revision of the base model.
-		 * @param local
-		 *            Local resource currently being compared.
-		 */
-		public RevisionedURIHandler(IRepositoryResource revision, ILocalResource local) {
 			baseRevision = revision;
 			localResource = local;
 		}
@@ -171,11 +168,9 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 		/**
 		 * {@inheritDoc}
 		 * 
-		 * @see org.eclipse.emf.ecore.resource.impl.URIHandlerImpl#createInputStream(org.eclipse.emf.common.util.URI,
-		 *      java.util.Map)
+		 * @see org.eclipse.emf.ecore.resource.URIConverter#createInputStream(org.eclipse.emf.common.util.URI)
 		 */
-		@Override
-		public InputStream createInputStream(URI uri, Map<?, ?> options) {
+		public InputStream createInputStream(URI uri) {
 			try {
 				// We'll have to change the EMF URI to find the IFile it points to
 				URI deresolvedURI = uri;
@@ -223,42 +218,119 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 			}
 			return null;
 		}
-	}
-
-	/**
-	 * This will allow us to stream a revision's content to a String.
-	 * 
-	 * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
-	 * @since 0.9
-	 */
-	private class StringOutputStream extends OutputStream {
-		/** StringWriter towards which calls will be delegated. */
-		private final StringWriter writer = new StringWriter();
 
 		/**
-		 * Instantiates the OutputStream.
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.emf.ecore.resource.URIConverter#createOutputStream(org.eclipse.emf.common.util.URI)
 		 */
-		public StringOutputStream() {
-			// increases visibility
+		public OutputStream createOutputStream(URI uri) throws IOException {
+			try {
+				URL url = new URL(uri.toString());
+				final URLConnection urlConnection = url.openConnection();
+				urlConnection.setDoOutput(true);
+				if (urlConnection instanceof HttpURLConnection) {
+					final HttpURLConnection httpURLConnection = (HttpURLConnection)urlConnection;
+					httpURLConnection.setRequestMethod("PUT"); //$NON-NLS-1$
+					return new FilterOutputStream(urlConnection.getOutputStream()) {
+						@Override
+						public void close() throws IOException {
+							super.close();
+							int responseCode = httpURLConnection.getResponseCode();
+							switch (responseCode) {
+								case HttpURLConnection.HTTP_OK:
+								case HttpURLConnection.HTTP_CREATED:
+								case HttpURLConnection.HTTP_NO_CONTENT: {
+									break;
+								}
+								default: {
+									throw new IOException("PUT failed with HTTP response code " //$NON-NLS-1$
+											+ responseCode);
+								}
+							}
+						}
+					};
+				}
+				OutputStream result = urlConnection.getOutputStream();
+				return result;
+			} catch (RuntimeException exception) {
+				throw new Resource.IOWrappedException(exception);
+			}
 		}
 
 		/**
 		 * {@inheritDoc}
 		 * 
-		 * @see java.io.OutputStream#write(int)
+		 * @see org.eclipse.emf.ecore.resource.URIConverter#getURIMap()
 		 */
-		@Override
-		public void write(int b) throws IOException {
-			writer.write(b);
+		public Map<URI, URI> getURIMap() {
+			return getInternalURIMap();
 		}
 
 		/**
-		 * Returns the underlying StringWriter.
+		 * Returns the normalized form of the URI.
+		 * <p>
+		 * This implementation does precisely and only the {@link URIConverter#normalize typical} thing. It
+		 * calls itself recursively so that mapped chains are followed.
+		 * </p>
 		 * 
-		 * @return The underlying StringWriter.
+		 * @param uri
+		 *            the URI to normalize.
+		 * @return the normalized form.
+		 * @see org.eclipse.emf.ecore.plugin.EcorePlugin#getPlatformResourceMap
 		 */
-		public StringWriter getWriter() {
-			return writer;
+		public URI normalize(URI uri) {
+			String fragment = uri.fragment();
+			URI result = fragment == null ? getInternalURIMap().getURI(uri) : getInternalURIMap().getURI(
+					uri.trimFragment()).appendFragment(fragment);
+			String scheme = result.scheme();
+			if (scheme == null) {
+				if (workspaceRoot != null) {
+					if (result.hasAbsolutePath()) {
+						result = URI.createPlatformResourceURI(result.trimFragment().toString(), false);
+						if (fragment != null) {
+							result = result.appendFragment(fragment);
+						}
+					}
+				} else {
+					if (result.hasAbsolutePath()) {
+						result = URI.createURI("file:" + result); //$NON-NLS-1$
+					} else {
+						result = URI.createFileURI(new File(result.trimFragment().toString())
+								.getAbsolutePath());
+						if (fragment != null) {
+							result = result.appendFragment(fragment);
+						}
+					}
+				}
+			}
+
+			if (result.equals(uri)) {
+				return uri;
+			}
+			return normalize(result);
+		}
+
+		/**
+		 * Returns the internal version of the URI map.
+		 * 
+		 * @return the internal version of the URI map.
+		 */
+		protected URIMap getInternalURIMap() {
+			if (uriMap == null) {
+				URIMappingRegistryImpl mappingRegistryImpl = new URIMappingRegistryImpl() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected URI delegatedGetURI(URI uri) {
+						return URIMappingRegistryImpl.INSTANCE.getURI(uri);
+					}
+				};
+
+				uriMap = (URIMap)mappingRegistryImpl.map();
+			}
+
+			return uriMap;
 		}
 	}
 
@@ -291,6 +363,42 @@ public class SubversiveTeamHandler extends AbstractTeamHandler {
 		public int read() throws IOException {
 			return reader.read();
 		}
+	}
 
+	/**
+	 * This will allow us to stream a revision's content to a String.
+	 * 
+	 * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
+	 * @since 0.9
+	 */
+	private class StringOutputStream extends OutputStream {
+		/** StringWriter towards which calls will be delegated. */
+		private final StringWriter writer = new StringWriter();
+
+		/**
+		 * Instantiates the OutputStream.
+		 */
+		public StringOutputStream() {
+			// increases visibility
+		}
+
+		/**
+		 * Returns the underlying StringWriter.
+		 * 
+		 * @return The underlying StringWriter.
+		 */
+		public StringWriter getWriter() {
+			return writer;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @see java.io.OutputStream#write(int)
+		 */
+		@Override
+		public void write(int b) throws IOException {
+			writer.write(b);
+		}
 	}
 }
