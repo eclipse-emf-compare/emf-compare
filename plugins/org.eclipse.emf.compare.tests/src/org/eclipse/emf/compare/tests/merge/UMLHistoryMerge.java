8c89d0f7dd912bd49113ccff913e65c0f5c5bf1a
lgoubet <laurent.goubet@obeo.fr>
2011-07-28 16:02:01 +0200

    Merging Cedric's changes

--- plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/UMLHistoryMerge.java
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/UMLHistoryMerge.java
@@ -1,7 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.emf.compare.tests.merge;
 
 import org.eclipse.emf.compare.tests.merge.data.EcoreMergeInputData;
 
+/**
+ * A Simple test case applying and undoing a real history of an existing uml.Ecore file.
+ * 
+ * @author <a href="mailto:cedric.brun@obeo.fr">Cedric Brun</a>
+ */
 public class UMLHistoryMerge extends MergeTestBase {
 
 	EcoreMergeInputData input = new EcoreMergeInputData();
