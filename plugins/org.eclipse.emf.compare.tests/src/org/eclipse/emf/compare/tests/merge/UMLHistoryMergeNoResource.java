9983a06418899b946139fb7e12defeb6e5f613d4
lgoubet <laurent.goubet@obeo.fr>
2011-08-01 16:57:12 +0200

    Prevent EMF Compare from failing in NPE when merging EObjects not contained in any resource. Providing a new algorithm for these cases : we copied the ResourceImpl#getEObject implementation to seek through the containment tree of the copied EObject's root container.

--- plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/UMLHistoryMergeNoResource.java
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/UMLHistoryMergeNoResource.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.emf.compare.tests.merge;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -22,21 +23,36 @@ import org.eclipse.emf.compare.diff.service.DiffService;
 import org.eclipse.emf.compare.match.metamodel.MatchModel;
 import org.eclipse.emf.compare.match.service.MatchService;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 
 public class UMLHistoryMergeNoResource extends UMLHistoryMerge {
+	Resource leftRes;
+
+	Resource rightRes;
 
 	@Override
 	protected List<DiffElement> detectDifferences(EObject left, EObject right) throws InterruptedException {
 
 		Map<String, Object> options = Collections.emptyMap();
 
-		left.eResource().getContents().clear();
-		right.eResource().getContents().clear();
+		leftRes = left.eResource();
+		rightRes = right.eResource();
+
+		leftRes.getContents().clear();
+		rightRes.getContents().clear();
 
 		MatchModel match = MatchService.doMatch(left, right, options);
 		DiffModel diff = DiffService.doDiff(match);
 
 		EList<DiffElement> differences = diff.getDifferences();
 		return differences;
+	}
+
+	@Override
+	protected void assertResult(boolean isLeftToRight, EObject testLeftModel, EObject testRightModel)
+			throws IOException {
+		leftRes.getContents().add(testLeftModel);
+		rightRes.getContents().add(testRightModel);
+		super.assertResult(isLeftToRight, testLeftModel, testRightModel);
 	}
 }
