8affc191638987c03db2726adbda0788c0faabd5
lgoubet <laurent.goubet@obeo.fr>
2011-08-03 11:46:24 +0200

    Providing the unit tests for the attribute changes

---
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/multivaluedattribute/MultiValuedAttributeMergeUseCases.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+package org.eclipse.emf.compare.tests.merge.multivaluedattribute;
+
+import org.eclipse.emf.compare.tests.merge.MergeTestBase;
+import org.eclipse.emf.compare.tests.merge.multivaluedattribute.data.MergeMMMultiValuedAttributeInputData;
+
+public class MultiValuedAttributeMergeUseCases extends MergeTestBase {
+	MergeMMMultiValuedAttributeInputData input = new MergeMMMultiValuedAttributeInputData();
+
+	public void testUseCase109() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase109());
+	}
+
+	public void testReverseUseCase109() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase109());
+	}
+
+	public void testUseCase110() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase110());
+	}
+
+	public void testReverseUseCase110() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase110());
+	}
+
+	public void testUseCase111() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase111());
+	}
+
+	public void testReverseUseCase111() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase111());
+	}
+
+	public void testUseCase112() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase112());
+	}
+
+	public void testReverseUseCase112() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase112());
+	}
+
+	public void testUseCase113() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase113());
+	}
+
+	public void testReverseUseCase113() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase113());
+	}
+
+	public void testUseCase114() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase114());
+	}
+
+	public void testReverseUseCase114() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase114());
+	}
+
+	public void testUseCase115() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase115());
+	}
+
+	public void testReverseUseCase115() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase115());
+	}
+
+	public void testUseCase116() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase116());
+	}
+
+	public void testReverseUseCase116() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase116());
+	}
+
+	public void testUseCase117() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase117());
+	}
+
+	public void testReverseUseCase117() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase117());
+	}
+
+	public void testUseCase118() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase118());
+	}
+
+	public void testReverseUseCase118() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase118());
+	}
+
+	public void testUseCase119() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase119());
+	}
+
+	public void testReverseUseCase119() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase119());
+	}
+
+	public void testUseCase120() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase120());
+	}
+
+	public void testReverseUseCase120() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase120());
+	}
+
+	public void testUseCase121() throws Exception {
+		mergeAllDiffsLeftToRight(input.getBase(), input.getUseCase121());
+	}
+
+	public void testReverseUseCase121() throws Exception {
+		mergeAllDiffsRightToLeft(input.getBase(), input.getUseCase121());
+	}
+}
---
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/multivaluedattribute/data/MergeMMMultiValuedAttributeInputData.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+package org.eclipse.emf.compare.tests.merge.multivaluedattribute.data;
+
+import java.io.IOException;
+
+import org.eclipse.emf.compare.tests.merge.data.AbstractInputData;
+import org.eclipse.emf.ecore.EObject;
+
+@SuppressWarnings("nls")
+public class MergeMMMultiValuedAttributeInputData extends AbstractInputData {
+	public EObject getBase() throws IOException {
+		String path = "base.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase109() throws IOException {
+		String path = "useCase109.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase110() throws IOException {
+		String path = "useCase110.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase111() throws IOException {
+		String path = "useCase111.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase112() throws IOException {
+		String path = "useCase112.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase113() throws IOException {
+		String path = "useCase113.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase114() throws IOException {
+		String path = "useCase114.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase115() throws IOException {
+		String path = "useCase115.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase116() throws IOException {
+		String path = "useCase116.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase117() throws IOException {
+		String path = "useCase117.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase118() throws IOException {
+		String path = "useCase118.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase119() throws IOException {
+		String path = "useCase119.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase120() throws IOException {
+		String path = "useCase120.merge";
+		return loadFromClassloader(path);
+	}
+
+	public EObject getUseCase121() throws IOException {
+		String path = "useCase121.merge";
+		return loadFromClassloader(path);
+	}
+}
