5e8c20980f5961c86d275fec8923c23bacf3a4ee
lgoubet <laurent.goubet@obeo.fr>
2011-08-05 16:36:42 +0200

    Regenerating test metamodel in order to introduce the new concepts needed for the "single-valued reference" merge use cases.

---
+++ plugins/org.eclipse.emf.compare.tests/src-gen/org/eclipse/emf/compare/tests/merge/impl/NodeMultiValueReferenceImpl.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+package org.eclipse.emf.compare.tests.merge.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.compare.tests.merge.MergePackage;
+import org.eclipse.emf.compare.tests.merge.Node;
+import org.eclipse.emf.compare.tests.merge.NodeMultiValueReference;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Node Multi Value Reference</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.eclipse.emf.compare.tests.merge.impl.NodeMultiValueReferenceImpl#getMultiValuedReference <em>Multi Valued Reference</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class NodeMultiValueReferenceImpl extends NodeImpl implements NodeMultiValueReference {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright (c) 2011 Obeo.\r\nAll rights reserved. This program and the accompanying materials\r\nare made available under the terms of the Eclipse Public License v1.0\r\nwhich accompanies this distribution, and is available at\r\nhttp://www.eclipse.org/legal/epl-v10.html\r\n\r\nContributors:\r\n    Obeo - initial API and implementation"; //$NON-NLS-1$
+
+	/**
+	 * The cached value of the '{@link #getMultiValuedReference() <em>Multi Valued Reference</em>}' reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getMultiValuedReference()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Node> multiValuedReference;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NodeMultiValueReferenceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MergePackage.Literals.NODE_MULTI_VALUE_REFERENCE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Node> getMultiValuedReference() {
+		if (multiValuedReference == null) {
+			multiValuedReference = new EObjectResolvingEList<Node>(Node.class, this, MergePackage.NODE_MULTI_VALUE_REFERENCE__MULTI_VALUED_REFERENCE);
+		}
+		return multiValuedReference;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case MergePackage.NODE_MULTI_VALUE_REFERENCE__MULTI_VALUED_REFERENCE:
+				return getMultiValuedReference();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case MergePackage.NODE_MULTI_VALUE_REFERENCE__MULTI_VALUED_REFERENCE:
+				getMultiValuedReference().clear();
+				getMultiValuedReference().addAll((Collection<? extends Node>)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case MergePackage.NODE_MULTI_VALUE_REFERENCE__MULTI_VALUED_REFERENCE:
+				getMultiValuedReference().clear();
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case MergePackage.NODE_MULTI_VALUE_REFERENCE__MULTI_VALUED_REFERENCE:
+				return multiValuedReference != null && !multiValuedReference.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //NodeMultiValueReferenceImpl
---
+++ plugins/org.eclipse.emf.compare.tests/src-gen/org/eclipse/emf/compare/tests/merge/impl/NodeSingleValueReferenceImpl.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ */
+package org.eclipse.emf.compare.tests.merge.impl;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.compare.tests.merge.MergePackage;
+import org.eclipse.emf.compare.tests.merge.Node;
+import org.eclipse.emf.compare.tests.merge.NodeSingleValueReference;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Node Single Value Reference</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.eclipse.emf.compare.tests.merge.impl.NodeSingleValueReferenceImpl#getSingleValuedReference <em>Single Valued Reference</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class NodeSingleValueReferenceImpl extends NodeImpl implements NodeSingleValueReference {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright (c) 2011 Obeo.\r\nAll rights reserved. This program and the accompanying materials\r\nare made available under the terms of the Eclipse Public License v1.0\r\nwhich accompanies this distribution, and is available at\r\nhttp://www.eclipse.org/legal/epl-v10.html\r\n\r\nContributors:\r\n    Obeo - initial API and implementation"; //$NON-NLS-1$
+
+	/**
+	 * The cached value of the '{@link #getSingleValuedReference() <em>Single Valued Reference</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getSingleValuedReference()
+	 * @generated
+	 * @ordered
+	 */
+	protected Node singleValuedReference;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NodeSingleValueReferenceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MergePackage.Literals.NODE_SINGLE_VALUE_REFERENCE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Node getSingleValuedReference() {
+		if (singleValuedReference != null && singleValuedReference.eIsProxy()) {
+			InternalEObject oldSingleValuedReference = (InternalEObject)singleValuedReference;
+			singleValuedReference = (Node)eResolveProxy(oldSingleValuedReference);
+			if (singleValuedReference != oldSingleValuedReference) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE, oldSingleValuedReference, singleValuedReference));
+			}
+		}
+		return singleValuedReference;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Node basicGetSingleValuedReference() {
+		return singleValuedReference;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setSingleValuedReference(Node newSingleValuedReference) {
+		Node oldSingleValuedReference = singleValuedReference;
+		singleValuedReference = newSingleValuedReference;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE, oldSingleValuedReference, singleValuedReference));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE:
+				if (resolve) return getSingleValuedReference();
+				return basicGetSingleValuedReference();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE:
+				setSingleValuedReference((Node)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE:
+				setSingleValuedReference((Node)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case MergePackage.NODE_SINGLE_VALUE_REFERENCE__SINGLE_VALUED_REFERENCE:
+				return singleValuedReference != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //NodeSingleValueReferenceImpl
