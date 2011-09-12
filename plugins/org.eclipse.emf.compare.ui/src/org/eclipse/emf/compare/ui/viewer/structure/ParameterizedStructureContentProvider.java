1d9fd06c89985d62069d9be258160d2dbae5c8b7
lgoubet <laurent.goubet@obeo.fr>
2011-09-12 11:26:25 +0200

    [357079] Enhancements for the filtering and grouping of differences.

--- plugins/org.eclipse.emf.compare.ui/src/org/eclipse/emf/compare/ui/viewer/structure/ParameterizedStructureContentProvider.java
+++ plugins/org.eclipse.emf.compare.ui/src/org/eclipse/emf/compare/ui/viewer/structure/ParameterizedStructureContentProvider.java
@@ -12,15 +12,20 @@ package org.eclipse.emf.compare.ui.viewer.structure;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.compare.diff.metamodel.AbstractDiffExtension;
 import org.eclipse.emf.compare.diff.metamodel.DiffElement;
+import org.eclipse.emf.compare.diff.metamodel.DiffGroup;
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.compare.ui.viewer.filter.IDifferenceFilter;
 import org.eclipse.emf.compare.ui.viewer.group.IDifferenceGroupingFacility;
 import org.eclipse.emf.compare.ui.viewer.group.IDifferenceGroupingFacility.UIDifferenceGroup;
+import org.eclipse.emf.ecore.EObject;
 
 /**
  * A content provider to manage the display of the difference elements in relation to the selected group and
@@ -85,7 +90,88 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		final Object[] result = groupElements(parentElement);
-		return filterElements(result);
+		return result;
+	}
+
+	/**
+	 * Almost the same as {@link ModelStructureContentProvider#getChildren(Object)} but call local
+	 * {@link #shouldBeHidden(EObject)} to take selected filters into account.
+	 * 
+	 * @param parentElement
+	 *            The element from which to retrieve all non-hidden children.
+	 * @return The list of non-hidden children of the given element.
+	 */
+	private Object[] getChildrenAsInSuper(Object parentElement) {
+		Object[] children = null;
+		if (parentElement instanceof EObject) {
+			final Collection<EObject> childrenList = new ArrayList<EObject>();
+			for (final EObject child : ((EObject)parentElement).eContents()) {
+				if (!shouldBeHidden(child)) {
+					childrenList.add(child);
+				}
+			}
+			children = childrenList.toArray();
+		}
+		return children;
+	}
+
+	/**
+	 * Specific should be hidden method taking into account the selected filters.
+	 * <p>
+	 * Almost the same as
+	 * {@link org.eclipse.emf.compare.diff.metamodel.util.DiffAdapterFactory#shouldBeHidden(EObject)} but call
+	 * {@link #isHidden(DiffElement)}.
+	 * </p>
+	 * 
+	 * @param element
+	 *            The element of which we need to check the "hidden" state.
+	 * @return <code>true</code> if the given element should be hidden, <code>false</code> otherwise.
+	 */
+	private static boolean shouldBeHidden(EObject element) {
+		boolean result = false;
+		if (element instanceof DiffElement) {
+			final DiffElement diff = (DiffElement)element;
+			final Iterator<AbstractDiffExtension> it = diff.getIsHiddenBy().iterator();
+			while (it.hasNext()) {
+				final AbstractDiffExtension extension = it.next();
+				if (!extension.isIsCollapsed()) {
+					result = true;
+				}
+			}
+			result = result || isHidden((DiffElement)element);
+		}
+		if (element instanceof DiffGroup) {
+			final DiffGroup group = (DiffGroup)element;
+			if (filteredSubchanges(group) == 0) {
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Like {@link DiffGroup#getSubDiffElements()} but take into account if sub-elements are filtered by
+	 * selected filters.
+	 * 
+	 * @param group
+	 *            The diff group for which we need to count the subchanges that are neither hidden nor
+	 *            filtered.
+	 * @return The count of changes that are neither hidden nor filtered.
+	 */
+	private static int filteredSubchanges(DiffGroup group) {
+		final Iterator<DiffElement> it = group.getSubDiffElements().iterator();
+		int result = 0;
+		while (it.hasNext()) {
+			final DiffElement eObj = it.next();
+			if (!shouldBeHidden(eObj)) {
+				if (eObj instanceof DiffGroup) {
+					result += filteredSubchanges((DiffGroup)eObj);
+				} else {
+					result += 1;
+				}
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -109,18 +195,27 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 		if (parentElement instanceof UIDifferenceGroup && mSelectedGroupFacility != null) {
 			final List<DiffElement> diffs = new ArrayList<DiffElement>();
 
-			if (input instanceof DiffModel) {
-				final Iterator<DiffElement> differences = ((DiffModel)input).getDifferences().iterator();
-				while (differences.hasNext()) {
-					final DiffElement diff = differences.next();
-					if (parentElement.equals(mSelectedGroupFacility.belongsTo(diff))) {
-						diffs.add(diff);
+			final Object[] differences = super.getElements(null);
+			for (int i = 0; i < differences.length; i++) {
+				final Object object = differences[i];
+				if (object instanceof DiffModel) {
+					final DiffModel diffModel = (DiffModel)object;
+					final EList<DiffElement> allDifferences = diffModel.getDifferences();
+					for (DiffElement diffElement : allDifferences) {
+						if (parentElement.equals(mSelectedGroupFacility.belongsTo(diffElement))) {
+							diffs.add(diffElement);
+						}
+					}
+				} else if (object instanceof DiffElement) {
+					final DiffElement diffElement = (DiffElement)object;
+					if (parentElement.equals(mSelectedGroupFacility.belongsTo(diffElement))) {
+						diffs.add(diffElement);
 					}
 				}
 			}
 			return diffs.toArray();
 		}
-		return super.getChildren(parentElement);
+		return getChildrenAsInSuper(parentElement);
 	}
 
 	/**
@@ -130,6 +225,7 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 	 *            The elements.
 	 * @return The filtered elements.
 	 */
+	@Deprecated
 	protected Object[] filterElements(Object[] elements) {
 		// TODO Get the right filters in relation to the menu checked
 		final List<Object> lResult = Arrays.asList(elements);
@@ -139,7 +235,7 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 		final Iterator<Object> itResult = lResult.iterator();
 		while (itResult.hasNext()) {
 			final Object obj = itResult.next();
-			if (obj instanceof DiffElement && isHidden((DiffElement)obj)) {
+			if (obj instanceof DiffElement && !(obj instanceof DiffGroup) && isHidden((DiffElement)obj)) {
 				filteredResult.remove(obj);
 			}
 		}
@@ -159,8 +255,11 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 			final Iterator<IDifferenceFilter> it = mSelectedFilters.iterator();
 			while (it.hasNext()) {
 				final IDifferenceFilter diffFilter = it.next();
-				if (diffFilter.hides(element))
+				// We do not support filtering of DiffGroup
+				// (as its DifferenceKind is always "Change" -- see DiffElementImpl#getKind() for how)
+				if (!(element instanceof DiffGroup) && diffFilter.hides(element)) {
 					return true;
+				}
 			}
 		}
 		return false;
@@ -175,6 +274,9 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 		final List<UIDifferenceGroup> result = new ArrayList<IDifferenceGroupingFacility.UIDifferenceGroup>();
 
 		if (mSelectedGroupFacility != null) {
+
+			initGroups();
+
 			final Iterator<UIDifferenceGroup> groups = mSelectedGroupFacility.allGroups().iterator();
 			while (groups.hasNext()) {
 				final UIDifferenceGroup group = groups.next();
@@ -185,6 +287,39 @@ public class ParameterizedStructureContentProvider extends ModelStructureContent
 		}
 
 		return result;
+	}
+
+	/**
+	 * Initialize the UI groups.
+	 */
+	private void initGroups() {
+		final Object[] elements = super.getElements(null);
+		for (int i = 0; i < elements.length; i++) {
+			final Object d = elements[i];
+			initChildrenGroups(d);
+
+			if (d instanceof DiffElement) {
+				mSelectedGroupFacility.belongsTo((DiffElement)d);
+			}
+		}
+	}
+
+	/**
+	 * Initialize the UI group for the given element and its own children.
+	 * 
+	 * @param inputElement
+	 *            The element for which to create UI groups.
+	 */
+	private void initChildrenGroups(Object inputElement) {
+		final Object[] elements = getChildrenAsInSuper(inputElement);
+		for (int i = 0; i < elements.length; i++) {
+			final Object d = elements[i];
+			initChildrenGroups(d);
+
+			if (d instanceof DiffElement) {
+				mSelectedGroupFacility.belongsTo((DiffElement)d);
+			}
+		}
 	}
 
 	/**
