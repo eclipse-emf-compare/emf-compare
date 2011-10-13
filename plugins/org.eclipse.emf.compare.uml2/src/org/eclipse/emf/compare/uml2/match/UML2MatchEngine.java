bd92007f9fc49fc6274ce6c72519ec3ac72160cf
lgoubet <laurent.goubet@obeo.fr>
2011-10-13 10:14:59 +0200

    [351593] Don't remove stereotype applications from the scope

--- plugins/org.eclipse.emf.compare.uml2/src/org/eclipse/emf/compare/uml2/match/UML2MatchEngine.java
+++ plugins/org.eclipse.emf.compare.uml2/src/org/eclipse/emf/compare/uml2/match/UML2MatchEngine.java
@@ -10,21 +10,14 @@
  *******************************************************************************/
 package org.eclipse.emf.compare.uml2.match;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.emf.compare.match.MatchOptions;
 import org.eclipse.emf.compare.match.engine.GenericMatchEngine;
-import org.eclipse.emf.compare.match.engine.GenericMatchScopeProvider;
 import org.eclipse.emf.compare.match.engine.IMatchScope;
 import org.eclipse.emf.compare.match.metamodel.MatchModel;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.uml2.uml.Element;
 
 /**
  * A specific IMatchEngine that ignores {@link EObject}s created to store UML stereotypes properties (a.k.a.
@@ -35,14 +28,16 @@ import org.eclipse.uml2.uml.Element;
  * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
  */
 public class UML2MatchEngine extends GenericMatchEngine {
-
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.compare.match.engine.GenericMatchEngine#resourceMatch(org.eclipse.emf.ecore.resource.Resource,
+	 *      org.eclipse.emf.ecore.resource.Resource, java.util.Map)
+	 */
 	@Override
 	public MatchModel resourceMatch(Resource leftResource, Resource rightResource,
 			Map<String, Object> optionMap) throws InterruptedException {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftResource,
-				rightResource));
-		return super.resourceMatch(leftResource, rightResource, uml2OptionsMap);
+		return super.resourceMatch(leftResource, rightResource, optionMap);
 	}
 
 	/**
@@ -54,10 +49,7 @@ public class UML2MatchEngine extends GenericMatchEngine {
 	@Override
 	public MatchModel contentMatch(EObject leftObject, EObject rightObject, EObject ancestor,
 			Map<String, Object> optionMap) {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftObject,
-				rightObject, ancestor));
-		return super.contentMatch(leftObject, rightObject, ancestor, uml2OptionsMap);
+		return super.contentMatch(leftObject, rightObject, ancestor, optionMap);
 	}
 
 	/**
@@ -68,10 +60,7 @@ public class UML2MatchEngine extends GenericMatchEngine {
 	 */
 	@Override
 	public MatchModel contentMatch(EObject leftObject, EObject rightObject, Map<String, Object> optionMap) {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftObject,
-				rightObject));
-		return super.contentMatch(leftObject, rightObject, uml2OptionsMap);
+		return super.contentMatch(leftObject, rightObject, optionMap);
 	}
 
 	/**
@@ -83,10 +72,7 @@ public class UML2MatchEngine extends GenericMatchEngine {
 	@Override
 	public MatchModel modelMatch(EObject leftRoot, EObject rightRoot, EObject ancestor,
 			Map<String, Object> optionMap) throws InterruptedException {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftRoot,
-				rightRoot));
-		return super.modelMatch(leftRoot, rightRoot, ancestor, uml2OptionsMap);
+		return super.modelMatch(leftRoot, rightRoot, ancestor, optionMap);
 	}
 
 	/**
@@ -98,10 +84,7 @@ public class UML2MatchEngine extends GenericMatchEngine {
 	@Override
 	public MatchModel modelMatch(EObject leftRoot, EObject rightRoot, Map<String, Object> optionMap)
 			throws InterruptedException {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftRoot,
-				rightRoot));
-		return super.modelMatch(leftRoot, rightRoot, uml2OptionsMap);
+		return super.modelMatch(leftRoot, rightRoot, optionMap);
 	}
 
 	/**
@@ -113,180 +96,17 @@ public class UML2MatchEngine extends GenericMatchEngine {
 	@Override
 	public MatchModel resourceMatch(Resource leftResource, Resource rightResource, Resource ancestorResource,
 			Map<String, Object> optionMap) throws InterruptedException {
-		final Map<String, Object> uml2OptionsMap = new HashMap<String, Object>(optionMap);
-		uml2OptionsMap.put(MatchOptions.OPTION_MATCH_SCOPE_PROVIDER, new UML2MatchScopeProvider(leftResource,
-				rightResource, ancestorResource));
-		return super.resourceMatch(leftResource, rightResource, ancestorResource, uml2OptionsMap);
+		return super.resourceMatch(leftResource, rightResource, ancestorResource, optionMap);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.compare.match.engine.GenericMatchEngine#getScopeInternalContents(org.eclipse.emf.ecore.EObject,
+	 *      org.eclipse.emf.compare.match.engine.IMatchScope)
+	 */
 	@Override
 	protected List<EObject> getScopeInternalContents(EObject eObject, IMatchScope scope) {
-		final List<EObject> result = new ArrayList<EObject>(super.getScopeInternalContents(eObject, scope));
-		// All is in the scope. See Bug 351593.
-		// result.addAll(getStereotypeApplications(eObject));
-		return result;
+		return super.getScopeInternalContents(eObject, scope);
 	}
-
-	/**
-	 * Get stereotype applications on the given model object.
-	 * 
-	 * @param eObject
-	 *            The model object.
-	 * @return List of stereotype applications.
-	 */
-	private List<EObject> getStereotypeApplications(EObject eObject) {
-		if (eObject instanceof Element) {
-			return ((Element)eObject).getStereotypeApplications();
-		} else {
-			return Collections.emptyList();
-		}
-	}
-
-	/**
-	 * UML2 match scope provider.
-	 * 
-	 * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
-	 */
-	private static class UML2MatchScopeProvider extends GenericMatchScopeProvider {
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftObject
-		 *            The left model object.
-		 * @param rightObject
-		 *            The right model object.
-		 * @param ancestorObject
-		 *            The ancestor model object.
-		 */
-		public UML2MatchScopeProvider(EObject leftObject, EObject rightObject, EObject ancestorObject) {
-			super(leftObject, rightObject, ancestorObject);
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftObject
-		 *            The left model object.
-		 * @param rightObject
-		 *            The right model object.
-		 */
-		public UML2MatchScopeProvider(EObject leftObject, EObject rightObject) {
-			super(leftObject, rightObject);
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftResource
-		 *            The left resource.
-		 * @param rightResource
-		 *            The right resource.
-		 * @param ancestorResource
-		 *            The ancestor resource.
-		 */
-		public UML2MatchScopeProvider(Resource leftResource, Resource rightResource, Resource ancestorResource) {
-			super(leftResource, rightResource, ancestorResource);
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftResourceSet
-		 *            The left resource set.
-		 * @param rightResourceSet
-		 *            The right resource set.
-		 * @param ancestorResourceSet
-		 *            The ancestor resource set.
-		 */
-		public UML2MatchScopeProvider(ResourceSet leftResourceSet, ResourceSet rightResourceSet,
-				ResourceSet ancestorResourceSet) {
-			super(leftResourceSet, rightResourceSet, ancestorResourceSet);
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftResourceSet
-		 *            The left resource set.
-		 * @param rightResourceSet
-		 *            The right resource set.
-		 */
-		public UML2MatchScopeProvider(ResourceSet leftResourceSet, ResourceSet rightResourceSet) {
-			super(leftResourceSet, rightResourceSet);
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param leftResource
-		 *            The left resource.
-		 * @param rightResource
-		 *            The right resource.
-		 */
-		public UML2MatchScopeProvider(Resource leftResource, Resource rightResource) {
-			super(leftResource, rightResource);
-		}
-
-		@Override
-		public IMatchScope getLeftScope() {
-			return new UML2MatchScope(super.getLeftScope());
-		}
-
-		@Override
-		public IMatchScope getRightScope() {
-			return new UML2MatchScope(super.getRightScope());
-		}
-
-		@Override
-		public IMatchScope getAncestorScope() {
-			return new UML2MatchScope(super.getAncestorScope());
-		}
-
-	}
-
-	/**
-	 * The UML2 match scope.
-	 * 
-	 * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
-	 */
-	private static final class UML2MatchScope implements IMatchScope {
-
-		/**
-		 * parent match scope.
-		 */
-		private final IMatchScope fParentScope;
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param scope
-		 *            The match scope.
-		 */
-		private UML2MatchScope(IMatchScope scope) {
-			fParentScope = scope;
-		}
-
-		/**
-		 * {@inheritDoc}
-		 * 
-		 * @see org.eclipse.emf.compare.match.engine.IMatchScope#isInScope(org.eclipse.emf.ecore.EObject)
-		 */
-		public boolean isInScope(EObject eObject) {
-			// All is in the scope. See Bug 351593.
-			// boolean isStereotypeApplication = UMLUtil.getStereotype(eObject) != null;
-			return fParentScope.isInScope(eObject)/* && !isStereotypeApplication */;
-		}
-
-		/**
-		 * {@inheritDoc}
-		 * 
-		 * @see org.eclipse.emf.compare.match.engine.IMatchScope#isInScope(org.eclipse.emf.ecore.resource.Resource)
-		 */
-		public boolean isInScope(Resource resource) {
-			return fParentScope.isInScope(resource);
-		}
-
-	}
-
 }
