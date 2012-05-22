609ef39febef872130621a8ee4d36aa5578ca1ba
lgoubet <laurent.goubet@obeo.fr>
2012-05-22 11:32:05 +0200

    Can tycho find this main?

--- plugins/org.eclipse.emf.compare.uml2.tests/src/org/eclipse/emf/compare/uml2/diff/test/suite/AllUML2Tests.java
+++ plugins/org.eclipse.emf.compare.uml2.tests/src/org/eclipse/emf/compare/uml2/diff/test/suite/AllUML2Tests.java
@@ -12,7 +12,6 @@ package org.eclipse.emf.compare.uml2.diff.test.suite;
 
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
-import junit.textui.TestRunner;
 
 import org.eclipse.emf.compare.uml2.diff.test.TestClassDiagram;
 import org.eclipse.emf.compare.uml2.diff.test.TestCommunicationDiagram;
@@ -31,18 +30,8 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({TestClassDiagram.class, TestCommunicationDiagram.class, TestProfile.class,
-		TestSequenceDiagram.class, TestUseCaseDiagram.class,TestUMLEcore.class})
+		TestSequenceDiagram.class, TestUseCaseDiagram.class, TestUMLEcore.class})
 public class AllUML2Tests {
-
-	/**
-	 * Launches the test with the given arguments.
-	 * 
-	 * @param args
-	 *            Arguments of the testCase.
-	 */
-	public static void main(String[] args) {
-		TestRunner.run(suite());
-	}
 
 	/**
 	 * Creates the {@link junit.framework.TestSuite TestSuite} for all the test.
