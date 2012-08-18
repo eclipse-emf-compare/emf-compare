0d26794b19ea742f6f5af05fb9131213fda9b404
cbrun <cedric.brun@obeo.fr>
2012-08-18 23:06:23 +0200

    UI Tests with SWTBots
    Adds two classes of tests, Ecore and UML.
    
    UML is failing because of an bug in the current trunk.
    Ecore is supposed to sucess but failing on my station with a
    noclassdeffounderror which makes me think its trying to do something
    with EMF Compare 1.x which is installed in my dev environnement.

---
+++ plugins/org.eclipse.emf.compare.ide.ui.tests/src/org/eclipse/emf/compare/ide/ui/tests/unit/UMLComparisonTest.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.compare.ide.ui.tests.unit;
+
+import org.eclipse.emf.compare.ide.ui.tests.automation.CompareBot;
+import org.eclipse.swtbot.swt.finder.SWTBotTestCase;
+
+public class UMLComparisonTest extends SWTBotTestCase {
+
+	private CompareBot bot = new CompareBot();
+
+	public void testCompareWithEachOther() {
+		bot.closeWelcome();
+		bot.importTestProjecstInWorkspace("UMLComparison");
+		bot.compareWithEachOther("UMLComparison", "v1.uml", "v2.uml");
+		// bot.createGITRepository("UMLComparison");
+
+		// bot.commit("UMLComparison", "v1Refv2.uml");
+		// System.out.println("WorkspaceComparisonTest.test()");
+	}
+
+}
