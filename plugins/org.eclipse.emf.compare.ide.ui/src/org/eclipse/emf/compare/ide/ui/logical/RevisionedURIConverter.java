8bba6b688228a0cd5d409abfb2978d806085be01
lgoubet <laurent.goubet@obeo.fr>
2012-10-19 10:37:41 +0200

    Load the right versions of the distant files.

--- plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/logical/RevisionedURIConverter.java
+++ plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/logical/RevisionedURIConverter.java
@@ -19,10 +19,13 @@ import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFileState;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
@@ -36,6 +39,28 @@ import org.eclipse.team.core.history.IFileRevision;
 /**
  * This {@link URIConverter} will be used in order to fetch remote contents instead of local contents when
  * loading resources.
+ * <p>
+ * Let's suppose we are trying to compare "ecore.genmodel". This file depends on "ecore.ecore". However,
+ * "ecore.ecore" evolves more than its corresponding genmodel since all changes made in this file are not
+ * "breaking" for the genmodel. For example, version <i>1.19</i> of the genmodel was commited at the same time
+ * as version <i>1.17</i> of the ecore. If I compare my own copy of the genmodel with the "latest from HEAD"
+ * (version <i>1.19</i>), then this URI Converter will be used to resolve the proxy of that "remote" file with
+ * the corresponding "remote" version of the ecore file. In this case, it should be version <i>1.20</i>
+ * (latest non-breaking) of the ecore file, not the <i>1.17</i> that was commited along.
+ * </p>
+ * <p>
+ * To this end, when creating a revisioned URI converter, we give it a file revision that corresponds to what
+ * the user asked to compare, in the above example, the "latest from HEAD" of ecore.genmodel. We will fetch
+ * the revision that is "above" that one. In the case of the "latest", there is none. if we had been given
+ * version <i>1.18</i>, we would consider version <i>1.19</i>... And consider this "younger" revision to be
+ * the "upper bound" of the revisions we'll resolve for dependent fragments (in the example above,
+ * ecore.ecore).
+ * </p>
+ * <p>
+ * This resolution strategy means that we assume the users never broke their logical model by committing some
+ * fragments without the others (i.e. they never committed "ecore.ecore" without committing the
+ * "ecore.genmodel" if there were breaking changes made).
+ * </p>
  * 
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
@@ -43,6 +68,20 @@ import org.eclipse.team.core.history.IFileRevision;
 public final class RevisionedURIConverter extends StorageURIConverter {
 	/** The revision of the base resource. This revision's timestamp will be used to resolve proxies. */
 	private IFileRevision baseRevision;
+
+	/** The actual Ifile from which was extracted {@link #baseRevision}. */
+	private IFile baseFile;
+
+	/**
+	 * We need a "maximum" timestamp : see javadoc of the class.
+	 * <p>
+	 * <ul>
+	 * <li><code>-2</code> is the unitialized value,</li>
+	 * <li><code>-1</code> means we have no upper bound (use the latest revisions of the files),</li>
+	 * <li>Any other value will be used to find the file revisions of our dependencies.</li>
+	 * </p>
+	 */
+	private long maxTimestamp = -2;
 
 	/**
 	 * Instantiates our URI converter given its delegate.
@@ -55,10 +94,36 @@ public final class RevisionedURIConverter extends StorageURIConverter {
 	public RevisionedURIConverter(URIConverter delegate, IFileRevision baseRevision) {
 		super(delegate);
 		this.baseRevision = baseRevision;
+		this.baseFile = findFile(getFileURI(baseRevision).toString());
+
 		try {
 			getLoadedRevisions().add(baseRevision.getStorage(new NullProgressMonitor()));
 		} catch (CoreException e) {
 			// We cannot find the storage of our base revision?
+		}
+
+		final RepositoryProvider repositoryProvider = RepositoryProvider.getProvider(baseFile.getProject());
+		if (repositoryProvider == null) {
+			return;
+		}
+
+		final IFileHistoryProvider historyProvider = repositoryProvider.getFileHistoryProvider();
+		final IFileHistory history = historyProvider.getFileHistoryFor(baseFile, IFileHistoryProvider.NONE,
+				new NullProgressMonitor());
+		if (history != null) {
+			// We'll search for the revision "above" the given one in order to determine our upper bound
+			final IFileRevision[] revisions = history.getFileRevisions();
+			final String baseID = baseRevision.getContentIdentifier();
+
+			for (int i = 0; i < revisions.length && this.maxTimestamp == -2; i++) {
+				if (baseID.equals(revisions[i].getContentIdentifier())) {
+					if (i == 0) {
+						this.maxTimestamp = -1;
+					} else {
+						this.maxTimestamp = revisions[i - 1].getTimestamp();
+					}
+				}
+			}
 		}
 	}
 
@@ -140,17 +205,12 @@ public final class RevisionedURIConverter extends StorageURIConverter {
 					IFileHistoryProvider.NONE, new NullProgressMonitor());
 
 			if (history != null) {
-				/*
-				 * This file exists on the repository. Try and find a revision that is older than the base
-				 * revision, or the closest higher if no older revision can be found.
-				 */
+				// This file exists on the repository.
 				IFileRevision soughtRevision = null;
-				for (final IFileRevision revision : history.getFileRevisions()) {
-					if (revision.getTimestamp() <= baseRevision.getTimestamp()) {
-						soughtRevision = revision;
-						break;
-					} else if (soughtRevision == null
-							|| soughtRevision.getTimestamp() >= revision.getTimestamp()) {
+				final IFileRevision[] revisions = history.getFileRevisions();
+				for (int i = 0; i < revisions.length && soughtRevision == null; i++) {
+					final IFileRevision revision = revisions[i];
+					if (maxTimestamp < 0 || revision.getTimestamp() < maxTimestamp) {
 						soughtRevision = revision;
 					}
 				}
@@ -172,10 +232,11 @@ public final class RevisionedURIConverter extends StorageURIConverter {
 			// Search through local history.
 			try {
 				IFileState soughtState = null;
-				for (final IFileState state : ((IFile)actualFile).getHistory(new NullProgressMonitor())) {
-					if (state.getModificationTime() <= baseRevision.getTimestamp()) {
-						soughtState = state;
-						break;
+				final IFileState[] revisions = ((IFile)actualFile).getHistory(new NullProgressMonitor());
+				for (int i = 0; i < revisions.length && soughtState == null; i++) {
+					final IFileState revision = revisions[i];
+					if (maxTimestamp < 0 || revision.getModificationTime() < maxTimestamp) {
+						soughtState = revision;
 					}
 				}
 
@@ -202,26 +263,66 @@ public final class RevisionedURIConverter extends StorageURIConverter {
 	 * @return The IFile for the given path if we could find it, <code>null</code> otherwise.
 	 */
 	private IFile findFile(String path) {
-		final java.net.URI baseURI = baseRevision.getURI();
+		final java.net.URI baseURI = getFileURI(baseRevision);
 		java.net.URI targetURI = convertToURI(path);
 		java.net.URI relativizedURI = baseURI.relativize(targetURI);
-		IFile file = null;
-		if (!relativizedURI.equals(targetURI)) {
-			file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(relativizedURI.toString()));
+		final IPath relativizedPath;
+		if (relativizedURI.getPath().length() == 0) {
+			relativizedPath = new Path(targetURI.getPath());
 		} else {
-			// git has a tendency to add the name of the repository before the name of the project, even for
-			// platform:/resource
-			final int indexOfSeparator = path.indexOf('/', 1);
-			if (indexOfSeparator > 0) {
-				targetURI = convertToURI(path.substring(indexOfSeparator));
-				relativizedURI = baseURI.relativize(targetURI);
-				file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(relativizedURI.toString()));
+			relativizedPath = new Path(relativizedURI.getPath());
+		}
+		final String[] pathSegments = relativizedPath.segments();
+
+		/*
+		 * 'Repository' URIs cannot be found directly in the workspace : they are relative to the repository
+		 * location. Let's try to find the IProject in which this file is located ...
+		 */
+		final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject container = null;
+
+		int projectIndex = 0;
+		for (; projectIndex < pathSegments.length && (container == null || !container.exists()); projectIndex++) {
+			container = root.getProject(pathSegments[projectIndex]);
+		}
+
+		IFile file = null;
+		if (container != null && container.exists()) {
+			final IPath filePath = relativizedPath.removeFirstSegments(projectIndex);
+			file = container.getFile(filePath);
+		}
+
+		return file;
+	}
+
+	private java.net.URI getFileURI(IFileRevision revision) {
+		final java.net.URI baseURI = revision.getURI();
+		if (baseURI != null) {
+			return baseURI;
+		}
+
+		java.net.URI result = null;
+		try {
+			final IStorage storage = revision.getStorage(new NullProgressMonitor());
+			final String name = revision.getName();
+
+			IPath path = storage.getFullPath();
+			final String lastSegment = path.lastSegment();
+			if (!lastSegment.equals(name)) {
+				final int nameIndex = lastSegment.indexOf(name);
+				if (nameIndex != -1) {
+					path = path.removeLastSegments(1);
+					path = path.append(lastSegment.substring(0, nameIndex + name.length()));
+				}
 			}
+
+			result = new java.net.URI(path.toString());
+		} catch (CoreException e) {
+			// FIXME log
+		} catch (URISyntaxException e) {
+			// FIXME log
 		}
-		if (file != null) {
-			return file;
-		}
-		return null;
+		return result;
 	}
 
 	/**
