7cb444e8609c0b0fc9d2872256a10aafcbd8ed46
Laurent Goubet <laurent.goubet@obeo.fr>
2012-11-16 14:44:16 +0100

    Remove duplicate cross references from the test models.
    Duplicates cause some issues when merging since EMF Compare does not
    manage to insert all elements at the appropriate index when there are
    duplicates in the target list (for example when trying to insert an
    element in-between two duplicates).

--- plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/FuzzyTest.java
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/FuzzyTest.java
@@ -10,12 +10,17 @@
  */
 package org.eclipse.emf.compare.tests;
 
+import com.google.common.collect.Sets;
+
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.EMFCompare;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.emfstore.fuzzy.Annotations.Data;
@@ -61,9 +66,11 @@ public class FuzzyTest {
 	@Test
 	public void copyAllRightToLeft() {
 		Assert.assertNotNull(root);
+		removeAllDuplicateCrossReferencesFrom(root);
 		EObject backup = EcoreUtil.copy(root);
 
 		util.mutate(createConfig());
+		removeAllDuplicateCrossReferencesFrom(root);
 
 		Comparison result = EMFCompare.builder().build().compare(EMFCompare.createDefaultScope(root, backup));
 		int nbDiffs = result.getDifferences().size();
@@ -85,9 +92,11 @@ public class FuzzyTest {
 	@Test
 	public void copyAllLeftToRight() {
 		Assert.assertNotNull(root);
+		removeAllDuplicateCrossReferencesFrom(root);
 		EObject backup = EcoreUtil.copy(root);
 
 		util.mutate(createConfig());
+		removeAllDuplicateCrossReferencesFrom(root);
 
 		Comparison result = EMFCompare.builder().build().compare(EMFCompare.createDefaultScope(root, backup));
 		int nbDiffs = result.getDifferences().size();
@@ -105,5 +114,26 @@ public class FuzzyTest {
 		ModelMutatorConfiguration conf = new ModelMutatorConfiguration(EcorePackage.eINSTANCE, root, 1L);
 		conf.setMaxDeleteCount(0);
 		return conf;
+	}
+
+	private static void removeAllDuplicateCrossReferencesFrom(EObject contentRoot) {
+		for (EReference reference : contentRoot.eClass().getEAllReferences()) {
+			if (!reference.isContainment() && !reference.isDerived() && reference.isMany()) {
+				@SuppressWarnings("unchecked")
+				final Iterator<EObject> crossReferences = ((List<EObject>)contentRoot.eGet(reference))
+						.iterator();
+				final Set<EObject> noDupes = Sets.newHashSet();
+				while (crossReferences.hasNext()) {
+					if (!noDupes.add(crossReferences.next())) {
+						crossReferences.remove();
+					}
+				}
+			}
+		}
+
+		final Iterator<EObject> contentIterator = contentRoot.eContents().iterator();
+		while (contentIterator.hasNext()) {
+			removeAllDuplicateCrossReferencesFrom(contentIterator.next());
+		}
 	}
 }
