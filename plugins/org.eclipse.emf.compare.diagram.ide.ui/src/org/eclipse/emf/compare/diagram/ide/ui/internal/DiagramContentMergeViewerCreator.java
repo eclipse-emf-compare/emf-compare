5eedf34d1f1de1dd193a24b029b469aa1953166b
Mikaël Barbero <mikael.barbero@obeo.fr>
2013-08-14 14:54:51 +0200

    Do not contribute more than one content merge viewer for Diagrams. (cherry picked from commit e731fc2b441f5616e7025c3082b1b4d66c680465)
    Conflicts:
    	plugins/org.eclipse.emf.compare.diagram.ide.ui/plugin.xml

---
+++ plugins/org.eclipse.emf.compare.diagram.ide.ui/src/org/eclipse/emf/compare/diagram/ide/ui/internal/DiagramContentMergeViewerCreator.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.emf.compare.diagram.ide.ui.internal;
+
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.IViewerCreator;
+import org.eclipse.emf.compare.diagram.ide.ui.internal.contentmergeviewer.diagram.DiagramContentMergeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * A {@link IViewerCreator} that creates {@link DiagramContentMergeViewer}.
+ * 
+ * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
+ */
+public class DiagramContentMergeViewerCreator implements IViewerCreator {
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.compare.IViewerCreator#createViewer(org.eclipse.swt.widgets.Composite,
+	 *      org.eclipse.compare.CompareConfiguration)
+	 */
+	public Viewer createViewer(Composite parent, CompareConfiguration config) {
+		return new DiagramContentMergeViewer(parent, config);
+	}
+
+}
