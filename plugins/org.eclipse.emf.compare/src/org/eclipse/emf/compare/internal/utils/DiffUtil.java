eclipse-emf-compare/emf-compare

commit a36c7e21f7243a0019d1a63cd2d997ec4a2d5826

Fix potential IndexOutOfBoundException

plugins/org.eclipse.emf.compare/src/org/eclipse/emf/compare/internal/utils/DiffUtil.java
@@ -648,20 +648,22 @@ public final class DiffUtil {
 			}
 		}
 
-		// Do we have duplicates before A in the lcs?
-		final Multiset<E> dupesLCS = HashMultiset.create(lcs.subList(0, lcsIndexOfSubsequenceStart + 1));
-		final E subsequenceStart = lcs.get(lcsIndexOfSubsequenceStart);
-		int duplicatesToGo = dupesLCS.count(subsequenceStart) - 1;
+		if (lcsIndexOfSubsequenceStart > 0) {
+			// Do we have duplicates before A in the lcs?
+			final Multiset<E> dupesLCS = HashMultiset.create(lcs.subList(0, lcsIndexOfSubsequenceStart + 1));
+			final E subsequenceStart = lcs.get(lcsIndexOfSubsequenceStart);
+			int duplicatesToGo = dupesLCS.count(subsequenceStart) - 1;
 
-		// Then, find the index of "A" in target
-		for (int i = 0; i < target.size() && insertionIndex == -1; i++) {
-			final E targetElement = target.get(i);
+			// Then, find the index of "A" in target
+			for (int i = 0; i < target.size() && insertionIndex == -1; i++) {
+				final E targetElement = target.get(i);
 
-			if (equalityHelper.matchingValues(targetElement, subsequenceStart)) {
-				if (duplicatesToGo > 0) {
-					duplicatesToGo--;
-				} else {
-					insertionIndex = i + 1;
+				if (equalityHelper.matchingValues(targetElement, subsequenceStart)) {
+					if (duplicatesToGo > 0) {
+						duplicatesToGo--;
+					} else {
+						insertionIndex = i + 1;
+					}
 				}
 			}
 		}

commit 2bee5f82b4f7baad5e3c46c657f6a93d02d51b7b

Fix regression introduced in a36c7e21f7243a0019d1a63cd2d997ec4a2d5826

plugins/org.eclipse.emf.compare/src/org/eclipse/emf/compare/internal/utils/DiffUtil.java
@@ -648,7 +648,7 @@ public final class DiffUtil {
 			}
 		}
 
-		if (lcsIndexOfSubsequenceStart > 0) {
+		if (lcsIndexOfSubsequenceStart > -1) {
 			// Do we have duplicates before A in the lcs?
 			final Multiset<E> dupesLCS = HashMultiset.create(lcs.subList(0, lcsIndexOfSubsequenceStart + 1));
 			final E subsequenceStart = lcs.get(lcsIndexOfSubsequenceStart);