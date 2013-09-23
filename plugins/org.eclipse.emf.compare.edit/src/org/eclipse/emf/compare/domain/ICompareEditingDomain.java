a361d39e80bbf8e2834358fdce93836a04780fa5
Mikaël Barbero <mikael.barbero@obeo.fr>
2013-09-23 16:21:29 +0200

    Add Javadoc to compare editing domain related classes
    Change-Id: I5ce7d7a8e2a6aeea1bace2e01deeb801061dfffc

--- plugins/org.eclipse.emf.compare.edit/src/org/eclipse/emf/compare/domain/ICompareEditingDomain.java
+++ plugins/org.eclipse.emf.compare.edit/src/org/eclipse/emf/compare/domain/ICompareEditingDomain.java
@@ -19,20 +19,42 @@ import org.eclipse.emf.compare.merge.IMerger;
 import org.eclipse.emf.ecore.change.util.ChangeRecorder;
 
 /**
+ * Something like EMF's {@link org.eclipse.emf.edit.domain.EditingDomain} but dedicated to the handling of
+ * comparison merging and undoing / redoing those merge.
+ * 
  * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
  */
 public interface ICompareEditingDomain {
 
 	void dispose();
 
+	/**
+	 * Returns the associated {@link ICompareCommandStack}.
+	 * 
+	 * @return the associated {@link ICompareCommandStack}.
+	 */
 	ICompareCommandStack getCommandStack();
 
 	/**
+	 * Returns the associated {@link ChangeRecorder}.
+	 * 
+	 * @return the associated {@link ChangeRecorder}.
 	 * @since 3.0
 	 */
 	ChangeRecorder getChangeRecorder();
 
 	/**
+	 * Creates a new command that will merge the given differences in from right to left or left to right by
+	 * using the mergers defined in the given merger registry.
+	 * 
+	 * @param differences
+	 *            the differences to merge.
+	 * @param leftToRight
+	 *            whether the merge has to be merge from left to right or right to left.
+	 * @param mergerRegistry
+	 *            the merger registry to query to get the appropriate mergers for each difference to be
+	 *            merged.
+	 * @return the created command.
 	 * @since 3.0
 	 */
 	Command createCopyCommand(List<? extends Diff> differences, boolean leftToRight,
--- plugins/org.eclipse.emf.compare.edit/src/org/eclipse/emf/compare/domain/impl/EMFCompareEditingDomain.java
+++ plugins/org.eclipse.emf.compare.edit/src/org/eclipse/emf/compare/domain/impl/EMFCompareEditingDomain.java
@@ -32,19 +32,43 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.change.util.ChangeRecorder;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 
 /**
+ * Default implementation that use a change recorder in the background to record the changes made by executed
+ * commands.
+ * 
  * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
  */
 public class EMFCompareEditingDomain implements ICompareEditingDomain {
 
+	/** The change recorder instance. */
 	private final ChangeRecorder fChangeRecorder;
 
+	/** The notifiers on which the change recorder will be installed. */
 	private final ImmutableCollection<Notifier> fNotifiers;
 
+	/** The command stack on which the merge commands will be executed. */
 	private final ICompareCommandStack fCommandStack;
 
+	/**
+	 * Creates a new instance with the given notifiers to be listen to when something will be changed.
+	 * 
+	 * @param left
+	 *            the left root notifier of the comparison (i.e. the
+	 *            {@link org.eclipse.emf.compare.scope.IComparisonScope#getLeft()}
+	 * @param right
+	 *            the right root notifier of the comparison (i.e. the
+	 *            {@link org.eclipse.emf.compare.scope.IComparisonScope#getRight()}
+	 * @param ancestor
+	 *            the ancestor root notifier of the comparison (i.e. the
+	 *            {@link org.eclipse.emf.compare.scope.IComparisonScope#getOrigin()}
+	 * @param commandStack
+	 *            the command stack to be used to track execution of commands.
+	 */
 	public EMFCompareEditingDomain(Notifier left, Notifier right, Notifier ancestor,
 			ICompareCommandStack commandStack) {
 		if (ancestor == null) {
@@ -59,6 +83,15 @@ public class EMFCompareEditingDomain implements ICompareEditingDomain {
 		fChangeRecorder.setResolveProxies(false);
 	}
 
+	/**
+	 * Returns the resource set containing the given notifier, the given {@code notifier} if it is a
+	 * {@link ResourceSet}, <code>null</code> otherwise.
+	 * 
+	 * @param notifier
+	 *            the notifier from which we have to look for a {@link ResourceSet}.
+	 * @return the resource set containing the given notifier, the given {@code notifier} if it is a
+	 *         {@link ResourceSet}, <code>null</code> otherwise
+	 */
 	private static ResourceSet getResourceSet(Notifier notifier) {
 		ResourceSet resourceSet = null;
 		if (notifier instanceof ResourceSet) {
@@ -71,50 +104,121 @@ public class EMFCompareEditingDomain implements ICompareEditingDomain {
 				resourceSet = eResource.getResourceSet();
 			}
 		} else {
-			// impossible as yet
+			// impossible as of today
 		}
 		return resourceSet;
 	}
 
+	/**
+	 * Creates a new compare editing domain on the given notifier with an appropriate
+	 * {@link ICompareCommandStack} set up on it.
+	 * 
+	 * @param left
+	 *            the left notifier. Should not be <code>null</code>.
+	 * @param right
+	 *            the right notifier. Should not be <code>null</code>.
+	 * @param ancestor
+	 *            the ancestor notifier. May be <code>null</code>.
+	 * @return a new compare editing domain on the given notifier.
+	 */
 	public static ICompareEditingDomain create(Notifier left, Notifier right, Notifier ancestor) {
-		final ResourceSet leftRS = getResourceSet(left);
-		final ResourceSet rightRS = getResourceSet(right);
+		EditingDomain leftED = getEditingDomain(left);
+		EditingDomain rightED = getEditingDomain(right);
 
-		final CommandStack commandStack;
-		if (leftRS != null && rightRS != null) {
-			TransactionalEditingDomain leftTED = TransactionalEditingDomain.Factory.INSTANCE
-					.getEditingDomain(leftRS);
-			TransactionalEditingDomain rightTED = TransactionalEditingDomain.Factory.INSTANCE
-					.getEditingDomain(rightRS);
-			if (leftTED == null) {
-				leftTED = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain(leftRS);
+		if (leftED != null && rightED != null) {
+			CommandStack leftCommandStack = leftED.getCommandStack();
+			CommandStack rightCommandStack = rightED.getCommandStack();
+			ICompareCommandStack commandStack;
+			if (leftCommandStack instanceof BasicCommandStack
+					&& rightCommandStack instanceof BasicCommandStack) {
+				commandStack = new DualCompareCommandStack((BasicCommandStack)leftCommandStack,
+						(BasicCommandStack)rightCommandStack);
+			} else {
+				// Shouldn't we raise an exception instead? The behavior will be strange.
+				commandStack = new CompareCommandStack(new BasicCommandStack());
 			}
-			if (rightTED == null) {
-				rightTED = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain(rightRS);
-			}
-			commandStack = new DualCompareCommandStack((BasicCommandStack)leftTED.getCommandStack(),
-					(BasicCommandStack)rightTED.getCommandStack());
-			return new EMFCompareEditingDomain(left, right, ancestor, (ICompareCommandStack)commandStack);
-		} else {
-			commandStack = new BasicCommandStack();
+			return new EMFCompareEditingDomain(left, right, ancestor, commandStack);
 		}
-		return create(left, right, ancestor, commandStack);
+
+		return create(left, right, ancestor, new BasicCommandStack());
 	}
 
+	/**
+	 * Returns an editing domain associated with the given {@link Notifier}. It will first look for a
+	 * {@link TransactionalEditingDomain} then for an {@link AdapterFactoryEditingDomain}. It neither is found
+	 * a new {@link TransactionalEditingDomain} is created.
+	 * 
+	 * @param notifier
+	 *            the notifier from which the editing domain has to be linked.
+	 * @return an editing domain associated with the given {@link Notifier}
+	 */
+	private static EditingDomain getEditingDomain(Notifier notifier) {
+		EditingDomain editingDomain = TransactionUtil.getEditingDomain(notifier);
+		if (editingDomain == null) {
+			editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(notifier);
+			if (editingDomain == null) {
+				ResourceSet resourceSet = getResourceSet(notifier);
+				if (resourceSet != null) {
+					editingDomain = TransactionalEditingDomain.Factory.INSTANCE
+							.createEditingDomain(resourceSet);
+				}
+			}
+		}
+
+		return editingDomain;
+	}
+
+	/**
+	 * Equivalent to {@code create(left, right, ancestor, commandStack, null)}.
+	 * 
+	 * @param left
+	 *            the left notifier. Should not be <code>null</code>.
+	 * @param right
+	 *            the right notifier. Should not be <code>null</code>.
+	 * @param ancestor
+	 *            the ancestor notifier. May be <code>null</code>.
+	 * @param commandStack
+	 *            a command stack to which merge command will be delegated to.
+	 * @return a newly created compare editing domain.
+	 */
 	public static ICompareEditingDomain create(Notifier left, Notifier right, Notifier ancestor,
 			CommandStack commandStack) {
 		return create(left, right, ancestor, commandStack, null);
 	}
 
+	/**
+	 * Creates a new compare editing domain on the given notifier with an appropriate
+	 * {@link ICompareCommandStack} set up on it.
+	 * 
+	 * @param left
+	 *            the left notifier. Should not be <code>null</code>.
+	 * @param right
+	 *            the right notifier. Should not be <code>null</code>.
+	 * @param ancestor
+	 *            the ancestor notifier. May be <code>null</code>.
+	 * @param leftCommandStack
+	 *            a command stack to which merge to left command will be delegated to.
+	 * @param rightCommandStack
+	 *            a command stack to which merge to irght command will be delegated to.
+	 * @return a newly created compare editing domain.
+	 */
 	public static ICompareEditingDomain create(Notifier left, Notifier right, Notifier ancestor,
 			CommandStack leftCommandStack, CommandStack rightCommandStack) {
 
 		final ICompareCommandStack commandStack;
 
 		if (leftCommandStack == null && rightCommandStack != null) {
-			commandStack = new CompareCommandStack(rightCommandStack);
+			if (rightCommandStack instanceof ICompareCommandStack) {
+				commandStack = (ICompareCommandStack)rightCommandStack;
+			} else {
+				commandStack = new CompareCommandStack(rightCommandStack);
+			}
 		} else if (leftCommandStack != null && rightCommandStack == null) {
-			commandStack = new CompareCommandStack(leftCommandStack);
+			if (leftCommandStack instanceof ICompareCommandStack) {
+				commandStack = (ICompareCommandStack)leftCommandStack;
+			} else {
+				commandStack = new CompareCommandStack(leftCommandStack);
+			}
 		} else if (leftCommandStack instanceof BasicCommandStack
 				&& rightCommandStack instanceof BasicCommandStack) {
 			commandStack = new DualCompareCommandStack((BasicCommandStack)leftCommandStack,
