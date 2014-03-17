d014f0ef1a5233d955f2f26bef2d175e014e8825
Arthur Daussy <arthur.daussy@obeo.fr>
2014-03-17 10:24:00 +0100

    Remove useless preference tab.
    Remove Filter and Group tab in editor preference pages since they have
    their on tab.
    
    Bug: 430372, 429636
    Change-Id: I5697e9be7360b1f198b6f4d8b38cf686900dec66
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- plugins/org.eclipse.emf.compare.rcp.ui/src/org/eclipse/emf/compare/rcp/ui/internal/preferences/EditorPreferencePage.java
+++ plugins/org.eclipse.emf.compare.rcp.ui/src/org/eclipse/emf/compare/rcp/ui/internal/preferences/EditorPreferencePage.java
@@ -15,13 +15,10 @@ import org.eclipse.emf.compare.rcp.ui.internal.mergeviewer.CompareColorImpl;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.dialogs.PreferenceLinkArea;
@@ -52,38 +49,10 @@ public class EditorPreferencePage extends PreferencePage implements IWorkbenchPr
 	@Override
 	protected Control createContents(Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
-		container.setLayout(new FillLayout(SWT.HORIZONTAL));
-		TabFolder tabFolder = new TabFolder(container, SWT.NONE);
-
-		createContentInMainTab(tabFolder);
-
-		createContentInFiltersTab(tabFolder);
-
-		createContentInGroupsTab(tabFolder);
-
-		return container;
-	}
-
-	private void createContentInGroupsTab(TabFolder tabFolder) {
-		TabItem tbtmGroups = new TabItem(tabFolder, SWT.NONE);
-		tbtmGroups.setText(EMFCompareRCPUIMessages.getString("EditorPreferencePage.groupsTab.label")); //$NON-NLS-1$
-	}
-
-	private void createContentInFiltersTab(TabFolder tabFolder) {
-		TabItem tbtmFilters = new TabItem(tabFolder, SWT.NONE);
-		tbtmFilters.setText(EMFCompareRCPUIMessages.getString("EditorPreferencePage.filtersTab.label")); //$NON-NLS-1$
-	}
-
-	private void createContentInMainTab(TabFolder tabFolder) {
-		TabItem tbtmMain = new TabItem(tabFolder, SWT.NONE);
-		tbtmMain.setText(EMFCompareRCPUIMessages.getString("EditorPreferencePage.mainTab.label")); //$NON-NLS-1$
-
-		Composite composite = new Composite(tabFolder, SWT.NONE);
-		composite.setLayout(new GridLayout(1, false));
-		tbtmMain.setControl(composite);
+		container.setLayout(new GridLayout(1, false));
 		// Link to color tab
 		PreferenceLinkArea fileEditorsArea = new PreferenceLinkArea(
-				composite,
+				container,
 				SWT.NONE,
 				"org.eclipse.ui.preferencePages.ColorsAndFonts", EMFCompareRCPUIMessages.getString("EditorPreferencePage.mainTab.colorHyperLink.label"),//$NON-NLS-1$ //$NON-NLS-2$
 				(IWorkbenchPreferenceContainer)getContainer(), "selectColor:" //$NON-NLS-1$
@@ -91,6 +60,8 @@ public class EditorPreferencePage extends PreferencePage implements IWorkbenchPr
 
 		GridData data = new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL);
 		fileEditorsArea.getControl().setLayoutData(data);
+
+		return container;
 	}
 
 }
