e6783520df4f1dda654e04c596d04aaea23f7391
Arthur Daussy <arthur.daussy@obeo.fr>
2014-05-13 15:03:15 +0200

    EMF Compare tutorial: EXTlibrary plugins
    Resources needed for the tutorial
    
    Change-Id: Ifa35b0269ae0974e4b175c11e6bb4336ae31a729
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

---
+++ tutorial/org.eclipse.emf.examples.library/src/org/eclipse/emf/examples/extlibrary/util/EXTLibraryResourceFactoryImpl.java
@@ -0,0 +1,41 @@
+/**
+ */
+package org.eclipse.emf.examples.extlibrary.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource Factory</b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.examples.extlibrary.util.EXTLibraryResourceImpl
+ * @generated
+ */
+public class EXTLibraryResourceFactoryImpl extends ResourceFactoryImpl {
+	/**
+	 * Creates an instance of the resource factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EXTLibraryResourceFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		Resource result = new EXTLibraryResourceImpl(uri);
+		return result;
+	}
+
+} //EXTLibraryResourceFactoryImpl
---
+++ tutorial/org.eclipse.emf.examples.library/src/org/eclipse/emf/examples/extlibrary/util/EXTLibraryResourceImpl.java
@@ -0,0 +1,36 @@
+/**
+ */
+package org.eclipse.emf.examples.extlibrary.util;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * <!-- begin-user-doc --> The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.examples.extlibrary.util.EXTLibraryResourceFactoryImpl
+ * @generated
+ */
+public class EXTLibraryResourceImpl extends XMIResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public EXTLibraryResourceImpl(URI uri) {
+		super(uri);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl#useUUIDs()
+	 * @generated NOT
+	 */
+	@Override
+	protected boolean useUUIDs() {
+		return true;
+	}
+} // EXTLibraryResourceImpl
