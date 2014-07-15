a50db622a8f398aa9ce28ea14c2289b9b0646e08
Mikaël Barbero <mikael.barbero@obeo.fr>
2014-07-15 10:47:02 +0200

    let's try with Q7 1.3.13-RC1
    Change-Id: I8f238c128f4b37e2e86c0e156d3f0e3ca940c258

--- plugins/org.eclipse.emf.compare.q7.tests/createAUT.sh
+++ plugins/org.eclipse.emf.compare.q7.tests/createAUT.sh
@@ -6,7 +6,7 @@ simrel=$4
 
 env > env.txt
 
-P2_ADMIN_VERSION="1.0.2"
+P2_ADMIN_VERSION="1.1.0"
 P2_ADMIN_ZIPNAME="p2-admin-$P2_ADMIN_VERSION-$os.$ws.$arch.tar.gz"
 P2_ADMIN_URL="https://github.com/mbarbero/p2-admin/releases/download/v$P2_ADMIN_VERSION/$P2_ADMIN_ZIPNAME"
 
@@ -30,24 +30,28 @@ if [[ $arch != "x86" ]]; then
 fi
 
 if [[ "$simrel" == "luna"* ]]; then
-	simrel_zip_name="eclipse-SDK-4.4-$os-$ws-$arch.tar.gz"
+	simrel_zip_name="eclipse-SDK-4.4-${target_env}.tar.gz"
 	simrel_zip_url="http://download.eclipse.org/eclipse/downloads/drops4/R-4.4-201406061215/$simrel_zip_name"
 	p2_repositories="http://download.eclipse.org/releases/luna/,\
 http://download.eclipse.org/modeling/emf/compare/updates/nightly/latest/,\
 http://download.eclipse.org/egit/updates-nightly"
 	p2_installIUs="org.eclipse.emf.compare.ide.ui.feature.group,\
 org.eclipse.egit.feature.group,\
+org.eclipse.emf.sdk.feature.group,\
+org.eclipse.uml2.uml.feature.group,\
+org.eclipse.papyrus.sdk.feature.feature.group,\
 org.eclipse.emf.compare.uml2.feature.group,\
 org.eclipse.emf.compare.diagram.gmf.feature.group,\
 org.eclipse.emf.compare.diagram.papyrus.feature.group"
 elif [[ "$simrel" == "kepler"* ]]; then
-	simrel_zip_name="eclipse-SDK-4.3.2-$os-$ws-$arch.tar.gz"
+	simrel_zip_name="eclipse-SDK-4.3.2-${target_env}.tar.gz"
 	simrel_zip_url="http://archive.eclipse.org/eclipse/downloads/drops4/R-4.3.2-201402211700/$simrel_zip_name"	
 	p2_repositories="http://download.eclipse.org/releases/kepler/,\
 http://download.eclipse.org/modeling/emf/compare/updates/nightly/latest/,\
 http://download.eclipse.org/egit/updates-nightly"
 	p2_installIUs="org.eclipse.emf.compare.ide.ui.feature.group,\
-org.eclipse.egit.feature.group"
+org.eclipse.egit.feature.group,
+org.eclipse.emf.sdk.feature.group"
 else
 	echo "Unknown 'simrel'=$simrel."
 	exit -1
