eclipse-emf-compare/emf-compare

commit 9f645815e56714165dd70b5c3d13e552896a3e58

Launch resolution of cross-resource proxies as soon as they're detected
Re-use the same trick as I1d90e784d7bcad7df43fa8ea0fb09a9b4fe3fb17 in
order to be notified of proxy creations and launch resolution threads as
soon as the proxies are detected instead of iterating post-loading, this
time during the logical model resolution phase.

Change-Id: I76bc1bc0b0facde75ab36be1aace82d05f4c7101
Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>

plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/internal/logical/resolver/ThreadedModelResolver.java
@@ -59,6 +59,7 @@ import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.ide.internal.utils.ProxyNotifierParserPool.IProxyCreationListener;
 import org.eclipse.emf.compare.ide.ui.internal.EMFCompareIDEUIMessages;
 import org.eclipse.emf.compare.ide.ui.internal.EMFCompareIDEUIPlugin;
 import org.eclipse.emf.compare.ide.ui.internal.preferences.EMFCompareUIPreferences;
@@ -72,6 +73,9 @@ import org.eclipse.emf.compare.ide.utils.ResourceUtil;
 import org.eclipse.emf.compare.ide.utils.StorageTraversal;
 import org.eclipse.emf.compare.ide.utils.StorageURIConverter;
 import org.eclipse.emf.compare.internal.utils.Graph;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -320,7 +324,8 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 			setupResolving();
 
 			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
-				final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet();
+				final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet(
+						new MonitoredProxyCreationListener(subMonitor, false));
 				updateDependencies(resourceSet, (IFile)start, subMonitor);
 				updateChangedResources(resourceSet, subMonitor);
 			}
@@ -402,7 +407,8 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 			setupResolving();
 
 			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
-				final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet();
+				final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet(
+						new MonitoredProxyCreationListener(monitor, false));
 				updateDependencies(resourceSet, (IFile)left, monitor);
 				updateDependencies(resourceSet, (IFile)right, monitor);
 				if (origin instanceof IFile) {
@@ -554,7 +560,8 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 		// Then load the same set of resources for the remote sides, completing it top-down
 
 		if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
-			final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet();
+			final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet(
+					new MonitoredProxyCreationListener(monitor, false));
 			updateDependencies(resourceSet, left, monitor);
 			updateChangedResources(resourceSet, monitor);
 		}
@@ -807,7 +814,8 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 	private Set<IStorage> findAdditionalRemoteTraversal(IStorageProviderAccessor storageAccessor,
 			Set<IStorage> alreadyLoaded, Set<IStorage> additionalStorages, DiffSide side,
 			ThreadSafeProgressMonitor monitor) throws InterruptedException {
-		final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet();
+		final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet(
+				new MonitoredProxyCreationListener(monitor, true));
 		final StorageURIConverter converter = new RevisionedURIConverter(resourceSet.getURIConverter(),
 				storageAccessor, side);
 		resourceSet.setURIConverter(converter);
@@ -1035,7 +1043,8 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 	private Set<IStorage> resolveRemoteTraversal(IStorageProviderAccessor storageAccessor, IStorage start,
 			Iterable<URI> knownVariants, DiffSide side, ThreadSafeProgressMonitor monitor)
 			throws InterruptedException {
-		final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet();
+		final SynchronizedResourceSet resourceSet = new SynchronizedResourceSet(
+				new MonitoredProxyCreationListener(monitor, true));
 		final StorageURIConverter converter = new RevisionedURIConverter(resourceSet.getURIConverter(),
 				storageAccessor, side);
 		resourceSet.setURIConverter(converter);
@@ -1362,6 +1371,33 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 		}
 	}
 
+	private class MonitoredProxyCreationListener implements IProxyCreationListener {
+		private final ThreadSafeProgressMonitor monitor;
+
+		private final boolean remote;
+
+		public MonitoredProxyCreationListener(ThreadSafeProgressMonitor monitor, boolean remote) {
+			this.monitor = monitor;
+			this.remote = remote;
+		}
+
+		public void proxyCreated(Resource source, EObject eObject, EStructuralFeature eStructuralFeature,
+				EObject proxy, int position) {
+			final URI from = source.getURI();
+			final URI to = ((InternalEObject)proxy).eProxyURI().trimFragment();
+
+			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
+				SynchronizedResourceSet resourceSet = (SynchronizedResourceSet)source.getResourceSet();
+				if (remote) {
+					demandRemoteResolve(resourceSet, to, monitor);
+				} else {
+					dependencyGraph.addChildren(from, Collections.singleton(to));
+					demandResolve(resourceSet, to, monitor);
+				}
+			}
+		}
+	}
+
 	/**
 	 * The callback for {@link ResourceResolver} and {@link RemoteResourceResolver} tasks. It will report
 	 * progress, log errors and finalize the resolving and as such, possibly signaling the end of the
@@ -1459,19 +1495,6 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 				safeMergeDiagnostic(resourceDiagnostic);
 			}
 			dependencyGraph.add(uri);
-			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
-				final Set<URI> crossReferencedResources = resourceSet.discoverCrossReferences(resource,
-						monitor);
-				dependencyGraph.addChildren(uri, crossReferencedResources);
-				for (URI crossRef : crossReferencedResources) {
-					if (isInterruptedOrCanceled(monitor)) {
-						demandResolvingAndUnloadingPoolShutdown();
-						// do not return, we want to unload what we've already loaded to avoid leaks.
-						break;
-					}
-					demandResolve(resourceSet, crossRef, monitor);
-				}
-			}
 			demandUnload(resourceSet, resource, monitor);
 		}
 	}
@@ -1520,18 +1543,6 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 			Diagnostic resourceDiagnostic = EcoreUtil.computeDiagnostic(resource, true);
 			if (resourceDiagnostic.getSeverity() >= Diagnostic.WARNING) {
 				safeMergeDiagnostic(resourceDiagnostic);
-			}
-			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
-				final Set<URI> crossReferencedResources = resourceSet.discoverCrossReferences(resource,
-						monitor);
-				for (URI crossRef : crossReferencedResources) {
-					if (isInterruptedOrCanceled(monitor)) {
-						demandResolvingAndUnloadingPoolShutdown();
-						// do not return, we want to unload what we've already loaded to avoid leaks.
-						break;
-					}
-					demandRemoteResolve(resourceSet, crossRef, monitor);
-				}
 			}
 			demandUnload(resourceSet, resource, monitor);
 		}

plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/internal/logical/resolver/SynchronizedResourceSet.java
@@ -15,29 +15,23 @@ import com.google.common.collect.Maps;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.AbstractEList;
-import org.eclipse.emf.common.util.AbstractTreeIterator;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.compare.ide.internal.utils.NoNotificationParserPool;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.compare.ide.internal.utils.ProxyNotifierParserPool.IProxyCreationListener;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.InternalEList;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 
 /**
@@ -50,18 +44,18 @@ class SynchronizedResourceSet extends ResourceSetImpl {
 	/** Associates URIs with their resources. */
 	private final ConcurrentHashMap<URI, Resource> uriCache;
 
-	public SynchronizedResourceSet() {
+	public SynchronizedResourceSet(IProxyCreationListener proxyListener) {
 		this.uriCache = new ConcurrentHashMap<URI, Resource>();
 		this.resources = new SynchronizedResourcesEList<Resource>();
 		this.loadOptions = super.getLoadOptions();
 		/*
 		 * This resource set is specifically designed to resolve cross resources links, it thus spends a lot
 		 * of time loading resources. The following set of options is what seems to give the most significant
-		 * boost in loading performances, though I did not fine-tune what's really needed here. Take note that
-		 * the use of our own parser pool along with the disabling of notifications is what allows us to
-		 * bypass UML's CacheAdapter and the potential dead locks it causes.
+		 * boost in loading performances, though I did not fine-tune what's really needed here.
 		 */
-		loadOptions.put(XMLResource.OPTION_USE_PARSER_POOL, new NoNotificationParserPool());
+		final NoNotificationParserPool parserPool = new NoNotificationParserPool();
+		parserPool.addProxyListener(proxyListener);
+		loadOptions.put(XMLResource.OPTION_USE_PARSER_POOL, parserPool);
 		loadOptions.put(XMLResource.OPTION_USE_DEPRECATED_METHODS, Boolean.FALSE);
 		loadOptions.put(XMLResource.OPTION_DISABLE_NOTIFY, Boolean.TRUE);
 
@@ -154,53 +148,6 @@ class SynchronizedResourceSet extends ResourceSetImpl {
 		}
 	}
 
-	/*
-	 * TODO this should be made into an extension point : give the user a Resource, and ask for a Set<URI> in
-	 * return, representing the cross references of said resource. Warn the client that implementation should
-	 * be kept fast and should avoid resolution of proxies.
-	 */
-	public Set<URI> discoverCrossReferences(Resource resource, IProgressMonitor monitor) {
-		resource.eSetDeliver(false);
-		final List<EObject> roots = ((InternalEList<EObject>)resource.getContents()).basicList();
-		final Iterator<EObject> resourceContent = roots.iterator();
-
-		final Set<URI> crossReferencedResources = new LinkedHashSet<URI>();
-		while (resourceContent.hasNext()) {
-
-			if (isInterruptedOrCanceled(monitor)) {
-				break;
-			}
-
-			final EObject eObject = resourceContent.next();
-			crossReferencedResources.addAll(resolveCrossReferences(eObject));
-			final TreeIterator<EObject> objectChildren = basicEAllContents(eObject);
-			while (objectChildren.hasNext()) {
-
-				if (isInterruptedOrCanceled(monitor)) {
-					break;
-				}
-
-				final EObject child = objectChildren.next();
-				if (child.eIsProxy()) {
-					final URI proxyURI = ((InternalEObject)child).eProxyURI().trimFragment();
-					crossReferencedResources.add(proxyURI);
-				} else {
-					crossReferencedResources.addAll(resolveCrossReferences(child));
-				}
-			}
-		}
-
-		if (isInterruptedOrCanceled(monitor)) {
-			crossReferencedResources.clear();
-		}
-
-		return crossReferencedResources;
-	}
-
-	private boolean isInterruptedOrCanceled(IProgressMonitor monitor) {
-		return monitor.isCanceled() || Thread.currentThread().isInterrupted();
-	}
-
 	public void unload(Resource resource, @SuppressWarnings("unused") IProgressMonitor monitor) {
 		final URI uri = resource.getURI();
 		uriCache.remove(uri);
@@ -211,52 +158,6 @@ class SynchronizedResourceSet extends ResourceSetImpl {
 		if (resource.getClass().getSimpleName().startsWith("UMLResource")) { //$NON-NLS-1$
 			resource.unload();
 		}
-	}
-
-	/**
-	 * An implementation of {@link EObject#eAllContents()} that will not resolve its proxies.
-	 * 
-	 * @param eObject
-	 *            The eobject for which contents we need an iterator.
-	 * @return The created {@link TreeIterator}.
-	 */
-	private TreeIterator<EObject> basicEAllContents(final EObject eObject) {
-		return new AbstractTreeIterator<EObject>(eObject, false) {
-			/** Generated SUID. */
-			private static final long serialVersionUID = -617740251257708686L;
-
-			/**
-			 * {@inheritDoc}
-			 * 
-			 * @see org.eclipse.emf.common.util.AbstractTreeIterator#getChildren(java.lang.Object)
-			 */
-			@Override
-			public Iterator<EObject> getChildren(Object obj) {
-				return ((InternalEList<EObject>)((EObject)obj).eContents()).basicIterator();
-			}
-		};
-	}
-
-	/**
-	 * Resolves the cross references of the given EObject, but leaves proxies as-is.
-	 * 
-	 * @param eObject
-	 *            The EObject for which we are to resolve the cross references.
-	 */
-	private Set<URI> resolveCrossReferences(EObject eObject) {
-		final Set<URI> crossReferencedResources = new LinkedHashSet<URI>();
-		final Iterator<EObject> objectCrossRefs = ((InternalEList<EObject>)eObject.eCrossReferences())
-				.basicIterator();
-		while (objectCrossRefs.hasNext()) {
-			final EObject crossRef = objectCrossRefs.next();
-			if (crossRef.eIsProxy()) {
-				final URI proxyURI = ((InternalEObject)crossRef).eProxyURI().trimFragment();
-				if (proxyURI.isPlatformResource()) {
-					crossReferencedResources.add(proxyURI);
-				}
-			}
-		}
-		return crossReferencedResources;
 	}
 
 	/**

plugins/org.eclipse.emf.compare.ide/src/org/eclipse/emf/compare/ide/internal/utils/NoNotificationParserPool.java
@@ -17,7 +17,6 @@ import org.eclipse.emf.ecore.xmi.XMLHelper;
 import org.eclipse.emf.ecore.xmi.XMLLoad;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMLHandler;
-import org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl;
 
 /**
  * This implementation of an {@link org.eclipse.emf.ecore.xmi.XMLParserPool} aims at disabling all
@@ -30,17 +29,11 @@ import org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl;
  * 
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
-public class NoNotificationParserPool extends XMLParserPoolImpl {
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl#getDefaultHandler(org.eclipse.emf.ecore.xmi.XMLResource,
-	 *      org.eclipse.emf.ecore.xmi.XMLLoad, org.eclipse.emf.ecore.xmi.XMLHelper, java.util.Map)
-	 */
+public class NoNotificationParserPool extends ProxyNotifierParserPool {
 	@Override
-	public synchronized XMLDefaultHandler getDefaultHandler(XMLResource resource, XMLLoad xmlLoad,
-			XMLHelper helper, Map<?, ?> options) {
-		final XMLDefaultHandler handler = super.getDefaultHandler(resource, xmlLoad, helper, options);
+	protected XMLDefaultHandler createDefaultHandler(XMLResource resource, XMLLoad xmlLoad, XMLHelper helper,
+			Map<?, ?> options) {
+		final XMLDefaultHandler handler = super.createDefaultHandler(resource, xmlLoad, helper, options);
 		if (handler instanceof XMLHandler) {
 			return new NoNotificationXMLHandler((XMLHandler)handler, resource, helper, options);
 		}

plugins/org.eclipse.emf.compare.ide/src/org/eclipse/emf/compare/ide/internal/utils/NotLoadingResourceSet.java
@@ -506,8 +506,8 @@ public final class NotLoadingResourceSet extends ResourceSetImpl implements Disp
 	/**
 	 * {@inheritDoc}
 	 */
-	public void proxyCreated(EObject eObject, EStructuralFeature eStructuralFeature, EObject proxy,
-			int position) {
+	public void proxyCreated(Resource source, EObject eObject, EStructuralFeature eStructuralFeature,
+			EObject proxy, int position) {
 		Set<EStructuralFeature> proxiesOn = proxyMap.get(eObject);
 		if (proxiesOn == null) {
 			proxiesOn = new LinkedHashSet<EStructuralFeature>();

plugins/org.eclipse.emf.compare.ide/src/org/eclipse/emf/compare/ide/internal/utils/ProxyNotifierParserPool.java
@@ -10,10 +10,10 @@
  *******************************************************************************/
 package org.eclipse.emf.compare.ide.internal.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
@@ -24,6 +24,7 @@ import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.ExtendedMetaData;
 import org.eclipse.emf.ecore.xmi.DanglingHREFException;
 import org.eclipse.emf.ecore.xmi.NameInfo;
@@ -43,14 +44,14 @@ import org.eclipse.emf.ecore.xmi.impl.XMLParserPoolImpl;
  */
 public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 	/** The list of parties interested by our proxies. */
-	private List<IProxyCreationListener> proxyListeners;
+	private ListenerList proxyListeners;
 
 	/**
 	 * Default constructor.
 	 */
 	public ProxyNotifierParserPool() {
 		super(true);
-		this.proxyListeners = new ArrayList<IProxyCreationListener>();
+		this.proxyListeners = new ListenerList();
 	}
 
 	/** {@inheritDoc} */
@@ -58,12 +59,32 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 	public synchronized XMLDefaultHandler getDefaultHandler(XMLResource resource, XMLLoad xmlLoad,
 			XMLHelper helper, Map<?, ?> options) {
 		final ProxyNotifierXMLHelper wrapper = new ProxyNotifierXMLHelper(helper);
-		for (IProxyCreationListener listener : proxyListeners) {
-			wrapper.addProxyListener(listener);
+		for (Object listener : proxyListeners.getListeners()) {
+			wrapper.addProxyListener((IProxyCreationListener)listener);
 		}
-		final XMLDefaultHandler handler = super.getDefaultHandler(resource, xmlLoad, wrapper, options);
+		final XMLDefaultHandler handler = createDefaultHandler(resource, xmlLoad, wrapper, options);
 		handler.prepare(resource, wrapper, options);
 		return handler;
+	}
+
+	/**
+	 * Create the default (unwrapped) XMLDefaultHandler. This is merely a call to <code>super</code> but can
+	 * be sub-classed.
+	 * 
+	 * @param resource
+	 *            The resource to load.
+	 * @param xmlLoad
+	 *            The XML load to pass on tho the handler.
+	 * @param helper
+	 *            The XML helper to pass on tho the handler.
+	 * @param options
+	 *            The load options for this resource.
+	 * @return The created XMLDefaultHandler.
+	 * @see #getDefaultHandler(XMLResource, XMLLoad, XMLHelper, Map)
+	 */
+	protected XMLDefaultHandler createDefaultHandler(XMLResource resource, XMLLoad xmlLoad, XMLHelper helper,
+			Map<?, ?> options) {
+		return super.getDefaultHandler(resource, xmlLoad, helper, options);
 	}
 
 	/**
@@ -94,6 +115,8 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 		/**
 		 * This will be called when a proxy is created from one of the parser pool's parsers.
 		 * 
+		 * @param source
+		 *            The resource in which a proxy has been created towards another.
 		 * @param eObject
 		 *            The EObject on which some feature is going to be set with a proxy value.
 		 * @param eStructuralFeature
@@ -105,7 +128,8 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 		 *            when the proxy is added at the end of the <code>eStructuralFeature</code>'s values list
 		 *            (for multi-valued features) or if said feature is single-valued.
 		 */
-		void proxyCreated(EObject eObject, EStructuralFeature eStructuralFeature, EObject proxy, int position);
+		void proxyCreated(Resource source, EObject eObject, EStructuralFeature eStructuralFeature,
+				EObject proxy, int position);
 	}
 
 	/**
@@ -114,7 +138,7 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 	 */
 	private static class ProxyNotifierXMLHelper extends ForwardingXMLHelper {
 		/** The list of parties interested by our proxy creations. */
-		private final List<IProxyCreationListener> proxyListeners;
+		private final ListenerList proxyListeners;
 
 		/**
 		 * Constructs a wrapper given its delegate XMLHelper.
@@ -124,7 +148,7 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 		 */
 		public ProxyNotifierXMLHelper(XMLHelper delegate) {
 			super(delegate);
-			this.proxyListeners = new ArrayList<IProxyCreationListener>();
+			this.proxyListeners = new ListenerList();
 		}
 
 		/** {@inheritDoc} */
@@ -133,8 +157,9 @@ public class ProxyNotifierParserPool extends XMLParserPoolImpl {
 				int position) {
 			super.setValue(eObject, eStructuralFeature, value, position);
 			if (value instanceof EObject && ((EObject)value).eIsProxy()) {
-				for (IProxyCreationListener listener : proxyListeners) {
-					listener.proxyCreated(eObject, eStructuralFeature, (EObject)value, position);
+				for (Object listener : proxyListeners.getListeners()) {
+					((IProxyCreationListener)listener).proxyCreated(getResource(), eObject,
+							eStructuralFeature, (EObject)value, position);
 				}
 			}
 		}

commit fd805b0175b0a585c9391b6f9fd293468a444aad

platform plugin and http should never be added to the dependency graph
This fixes a regression brought about by
9f645815e56714165dd70b5c3d13e552896a3e58 .

Change-Id: I66ae6c1385365f50769ff3389096e133aae61fda
Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>

plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/internal/logical/resolver/ThreadedModelResolver.java
@@ -1386,7 +1386,7 @@ public class ThreadedModelResolver extends AbstractModelResolver {
 			final URI from = source.getURI();
 			final URI to = ((InternalEObject)proxy).eProxyURI().trimFragment();
 
-			if (getResolutionScope() != CrossReferenceResolutionScope.SELF) {
+			if (getResolutionScope() != CrossReferenceResolutionScope.SELF && to.isPlatformResource()) {
 				SynchronizedResourceSet resourceSet = (SynchronizedResourceSet)source.getResourceSet();
 				if (remote) {
 					demandRemoteResolve(resourceSet, to, monitor);