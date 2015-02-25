1fad8b5123c35dcead82f5b0452403c0d7f14d7b
Axel Richard <axel.richard@obeo.fr>
2015-02-25 17:56:33 +0100

    [434621] RootIDMatchingStrategy matches incorrectly
    Avoid addition of null values in matching strategy.
    
    Bug: 434621
    Change-Id: Ia096ab45f849b315e9f05c28b837d9c13b515c17
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>

--- plugins/org.eclipse.emf.compare/src/org/eclipse/emf/compare/match/resource/RootIDMatchingStrategy.java
+++ plugins/org.eclipse.emf.compare/src/org/eclipse/emf/compare/match/resource/RootIDMatchingStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Obeo.
+ * Copyright (c) 2012, 2015 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -117,7 +117,10 @@ public class RootIDMatchingStrategy implements IResourceMatchingStrategy {
 					identifiers.add(resourceId);
 				}
 			}
-			identifiers.add(EcoreUtil.getID(root));
+			String rootID = EcoreUtil.getID(root);
+			if (rootID != null) {
+				identifiers.add(rootID);
+			}
 		}
 
 		return identifiers;
