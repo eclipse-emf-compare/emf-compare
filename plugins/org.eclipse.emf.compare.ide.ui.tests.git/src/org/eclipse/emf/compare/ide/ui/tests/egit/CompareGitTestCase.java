fdb80fa8b66fbef65e17f0c845d4050860dcd11b
Axel Richard <axel.richard@obeo.fr>
2015-09-11 09:27:31 +0200

    Taking into account new EGit preference about merge strategy in tests
    Change-Id: I34825a872dac4f024b72bbdb6e24fa69c9b591dc
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>

--- plugins/org.eclipse.emf.compare.ide.ui.tests.git/src/org/eclipse/emf/compare/ide/ui/tests/egit/CompareGitTestCase.java
+++ plugins/org.eclipse.emf.compare.ide.ui.tests.git/src/org/eclipse/emf/compare/ide/ui/tests/egit/CompareGitTestCase.java
@@ -65,6 +65,7 @@ public class CompareGitTestCase extends CompareTestCase {
 	public static void setUpClass() {
 		// suppress auto-ignoring and auto-sharing to avoid interference
 		IEclipsePreferences eGitPreferences = InstanceScope.INSTANCE.getNode(Activator.getPluginId());
+		eGitPreferences.put(GitCorePreferences.core_preferredMergeStrategy, "model recursive");
 		eGitPreferences.putBoolean(GitCorePreferences.core_autoShareProjects, false);
 		// This is actually the value of "GitCorePreferences.core_autoIgnoreDerivedResources"... but it was
 		// not in Egit 2.1
@@ -82,9 +83,8 @@ public class CompareGitTestCase extends CompareTestCase {
 		SystemReader.setInstance(mockSystemReader);
 		mockSystemReader.setProperty(Constants.GIT_CEILING_DIRECTORIES_KEY, ResourcesPlugin.getWorkspace()
 				.getRoot().getLocation().toFile().getParentFile().getAbsoluteFile().toString());
-		final IWorkspaceRoot workspaceRoot = project.getProject().getWorkspace().getRoot(); 
-		gitDir = new File(workspaceRoot.getRawLocation().toFile(),
-				Constants.DOT_GIT);
+		final IWorkspaceRoot workspaceRoot = project.getProject().getWorkspace().getRoot();
+		gitDir = new File(workspaceRoot.getRawLocation().toFile(), Constants.DOT_GIT);
 		repository = new GitTestRepository(gitDir);
 		repository.connect(project.getProject());
 		repository.ignore(workspaceRoot.getRawLocation().append(".metadata").toFile());
@@ -100,7 +100,7 @@ public class CompareGitTestCase extends CompareTestCase {
 		}
 		super.tearDown();
 	}
-	
+
 	protected void compareBothDirectionsAndCheck(IFile file, String source, String destination,
 			int expectedConflicts, int diffsInSource, int diffsInDestination) throws Exception {
 		Comparison compareResult = compare(source, destination, file);
@@ -113,10 +113,11 @@ public class CompareGitTestCase extends CompareTestCase {
 		assertEquals(expectedConflicts, compareResult.getConflicts().size());
 		assertDiffCount(compareResult.getDifferences(), diffsInDestination, diffsInSource);
 	}
-	
+
 	protected Comparison compare(String sourceRev, String targetRev, IFile file) throws Exception {
 		final String fullPath = file.getFullPath().toString();
-		final Subscriber subscriber = repository.createSubscriberForComparison(sourceRev, targetRev, file, false);
+		final Subscriber subscriber = repository.createSubscriberForComparison(sourceRev, targetRev, file,
+				false);
 		final IStorageProviderAccessor accessor = new SubscriberStorageAccessor(subscriber);
 		final IStorageProvider sourceProvider = accessor.getStorageProvider(file,
 				IStorageProviderAccessor.DiffSide.SOURCE);
@@ -150,20 +151,24 @@ public class CompareGitTestCase extends CompareTestCase {
 
 		final Builder comparisonBuilder = EMFCompare.builder();
 		EMFCompareBuilderConfigurator.createDefault().configure(comparisonBuilder);
-		
+
 		return comparisonBuilder.build().compare(scope, new BasicMonitor());
 	}
-	
-	protected IStorageProviderAccessor createAccessorForComparison(String sourceRev, String targetRev, boolean includeLocal) throws Exception {
-		final Subscriber subscriber = repository.createSubscriberForResolution(sourceRev, targetRev, includeLocal);
+
+	protected IStorageProviderAccessor createAccessorForComparison(String sourceRev, String targetRev,
+			boolean includeLocal) throws Exception {
+		final Subscriber subscriber = repository.createSubscriberForResolution(sourceRev, targetRev,
+				includeLocal);
 		return new SubscriberStorageAccessor(subscriber);
 	}
-	
-	protected IStorageProviderAccessor createRemoteAccessorForComparison(String sourceRev, String targetRev, IFile file) throws Exception {
-		final Subscriber subscriber = repository.createSubscriberForComparisonWithRemoteMappings(sourceRev, targetRev, file);
+
+	protected IStorageProviderAccessor createRemoteAccessorForComparison(String sourceRev, String targetRev,
+			IFile file) throws Exception {
+		final Subscriber subscriber = repository.createSubscriberForComparisonWithRemoteMappings(sourceRev,
+				targetRev, file);
 		return new SubscriberStorageAccessor(subscriber);
 	}
-	
+
 	protected static void assertDiffCount(List<Diff> differences, int expectedOutgoing, int expectedIncoming) {
 		assertEquals(expectedOutgoing + expectedIncoming, differences.size());
 
