e2355c833452144d9a79f89b66429738ffcfe0cb
Mathieu Cartaud <mathieu.cartaud@obeo.fr>
2015-09-22 10:16:16 +0200

    [478032] Fix accept/reject dropdown icon
    Bug: 478032
    Change-Id: I8003e90efb96cb3a14ec037b1799a3a29fe375fa
    Signed-off-by: Mathieu Cartaud <mathieu.cartaud@obeo.fr>

--- plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/internal/structuremergeviewer/EMFCompareStructureMergeViewer.java
+++ plugins/org.eclipse.emf.compare.ide.ui/src/org/eclipse/emf/compare/ide/ui/internal/structuremergeviewer/EMFCompareStructureMergeViewer.java
@@ -870,6 +870,13 @@ public class EMFCompareStructureMergeViewer extends AbstractStructuredViewerWrap
 		EMFCompareConfiguration compareConfiguration = getCompareConfiguration();
 		compareConfiguration.setLeftEditable(input.isLeftEditable());
 		compareConfiguration.setRightEditable(input.isRightEditable());
+
+		if (input.isLeftEditable() && input.isRightEditable()) {
+			compareConfiguration.setMergePreviewMode(MergeMode.RIGHT_TO_LEFT);
+		} else {
+			compareConfiguration.setMergePreviewMode(MergeMode.ACCEPT);
+		}
+
 		// setup defaults
 		if (compareConfiguration.getEditingDomain() == null) {
 			ICompareEditingDomain domain = EMFCompareEditingDomain.create(comparisonScope.getLeft(),
