8729a641e78b15269342124f3b375084c8afbea7
Martin Fleck <mfleck@eclipsesource.com>
2016-10-31 13:02:31 +0100

    [483798] Register Papyrus item provider for Papyrus comparisons
    - Papyrus context tester to determine Papyrus comparison
    - Papyrus item provider that reuses the Papyrus label provider services
    - Register Adapter factory decorator that returns the item provider
    - Remove old adapter factory decorator for Papyrus stereotyped elements 
    
    Bug: 483798
    Change-Id: I8e7769a25a9377bf6cd44e83ff99e05ff62098e4
    Signed-off-by: Martin Fleck <mfleck@eclipsesource.com>

--- plugins/org.eclipse.emf.compare.uml2.edit.papyrus/src/org/eclipse/emf/compre/uml2/edit/papyrus/internal/decorator/PapyrusElementFilteredLabelProvider.java
+++ plugins/org.eclipse.emf.compare.uml2.edit.papyrus/src/org/eclipse/emf/compre/uml2/edit/papyrus/internal/decorator/PapyrusElementFilteredLabelProvider.java
@@ -1,12 +1,14 @@
 /*****************************************************************************
- * Copyright (c) 2016 EclipseSource Services GmbH
+ * Copyright (c) 2016, 2017 EclipseSource Services GmbH
  * *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Alexandra Buzila (EclipseSource) - Initial API and implementation
+ * Contributors:
+ *      Alexandra Buzila (EclipseSource) - Initial API and implementation
+ *      Martin Fleck - bug 483798
  *****************************************************************************/
 
 package org.eclipse.emf.compre.uml2.edit.papyrus.internal.decorator;
@@ -14,6 +16,8 @@ package org.eclipse.emf.compre.uml2.edit.papyrus.internal.decorator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.papyrus.emf.facet.custom.core.ICustomizationManager;
 import org.eclipse.papyrus.emf.facet.custom.core.exception.CustomizationException;
+import org.eclipse.papyrus.emf.facet.custom.metamodel.custompt.IImage;
+import org.eclipse.papyrus.emf.facet.custom.ui.internal.ImageManager;
 import org.eclipse.papyrus.emf.facet.custom.ui.internal.PropertiesHandler;
 import org.eclipse.papyrus.infra.services.labelprovider.service.IFilteredLabelProvider;
 import org.eclipse.papyrus.uml.tools.providers.UMLLabelProvider;
@@ -29,9 +33,13 @@ public class PapyrusElementFilteredLabelProvider extends UMLLabelProvider implem
 	/** The properties handler. */
 	private PropertiesHandler propertiesHandler;
 
+	/** The image manager. */
+	private ImageManager imageManager;
+
 	/** Constructor. */
 	public PapyrusElementFilteredLabelProvider() {
 		this.propertiesHandler = new PropertiesHandler(getCustomizationManager());
+		this.imageManager = new ImageManager();
 	}
 
 	/**
@@ -69,8 +77,9 @@ public class PapyrusElementFilteredLabelProvider extends UMLLabelProvider implem
 			return super.getImage(element);
 		}
 		try {
-			return customizationManager.getCustomValueOf(element, propertiesHandler.getImageProperty(),
-					Image.class);
+			final IImage image = customizationManager.getCustomValueOf(element,
+					propertiesHandler.getImageProperty(), IImage.class);
+			return imageManager.getImage(image);
 		} catch (CustomizationException e) {
 			// fall back to super class
 			return super.getImage(element);
